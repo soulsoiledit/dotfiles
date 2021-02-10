b38c3c1f92465c14f87f59725fe007bc35aa83b9
soulsoil <creepachu21@gmail.com>
2021-02-10 14:28:52 -0600

    Added use_flake function for nix shells

--- home/modules/direnv.nix
+++ home/modules/direnv.nix
@@ -2,6 +2,14 @@
   programs.direnv = {
     enable = true;
     enableFishIntegration = true;
-    enableNixDirenvIntegration = true;
+    # enableNixDirenvIntegration = true;
+
+    stdlib = ''
+      use_flake() {
+        watch_file flake.nix
+        watch_file flake.lock
+        eval "$(nix print-dev-env --profile "$(direnv_layout_dir)/flake-profile")"
+      }
+    '';
   };
 }
