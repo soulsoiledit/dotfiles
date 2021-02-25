fdf0c90090ca8df88b40b811e9c552ffea147505
soulsoil <creepachu21@gmail.com>
2021-02-24 21:06:25 -0600

    Updated overlay to work with nix

--- home/modules/overlays.nix
+++ home/modules/overlays.nix
@@ -1,7 +1,14 @@
-[
-  # (self: super: {
-  #   discord = super.discord.overrideAttrs (_: {
-  #     src = builtins.fetchTarball https://discord.com/api/download?platform=linux&format=tar.gz;
-  #   });
-  # })
-]
+{ config, pkgs, ... }:
+
+{
+  nixpkgs.overlays = [
+    (self: super: {
+      discord = super.discord.overrideAttrs (_: {
+        src = builtins.fetchTarball {
+          url = https://discord.com/api/download?platform=linux&format=tar.gz;
+          sha256 = "0jps9dqjvzr2kg2xxcl0s3vmkpcnqnfa8m76r0n0prk239hpc63m";
+        };
+      });
+    })
+  ];
+}
