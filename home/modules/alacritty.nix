13ce06606614ba5ab426d3f559c310ba11b5d11a
soulsoiledit <creepachu21@gmail.com>
2021-03-29 20:24:57 -0500

    updated colorscheme and keybinds

--- home/modules/alacritty.nix
+++ home/modules/alacritty.nix
@@ -13,72 +13,76 @@
         size = 10;
         normal.family = "UbuntuMono Nerd Font";
       };
-
-      # miramare {{{
+      # gruvbox-material {{{
       colors = {
         primary = {
-          background = "#2a2426";
-          foreground = "#e6d6ac";
+          background = "#1d2021";
+          foreground = "#d4be98";
         };
 
         cursor = {
-          text = "#2a2426";
+          text = "#1d2021";
           cursor = "#e6d6ac";
         };
 
         vi_mode_cursor = {
-          text = "#2a2426";
           cursor = "#e6d6ac";
         };
 
         selection = {
-          background = "#444444";
+          background = "#3c3836";
           text = "CellForeground";
         };
 
         search = {
           matches = {
-            background = "#444444";
-            foreground = "CellForeground";
+            background = "#a9b665";
+            foreground = "#1d2021";
+          };
+          focused_match = {
+            background = "#ea6962";
+            foreground = "#1d2021";
           };
         };
 
         normal = {
-          black = "#242021";
-          red = "#e68183";
-          green = "#87af87";
-          yellow = "#d9bb80";
-          blue = "#89beba";
-          magenta = "#d3a0bc";
-          cyan = "#87c095";
-          white = "#e6d6ac";
+          black = "#504945";
+          red = "#ea6962";
+          green = "#a9b665";
+          yellow = "#d8a657";
+          blue = "#7daea3";
+          magenta = "#d3869b";
+          cyan = "#89b482";
+          white = "#d4be98";
         };
 
         bright = {
-          black = "#444444";
-          red = "#e68183";
-          green = "#87af87";
-          yellow = "#d9bb80";
-          blue = "#89beba";
-          magenta = "#d3a0bc";
-          cyan = "#87c095";
-          white = "#e6d6ac";
+          black = "#504945";
+          red = "#ea6962";
+          green = "#a9b665";
+          yellow = "#d8a657";
+          blue = "#7daea3";
+          magenta = "#d3869b";
+          cyan = "#89b482";
+          white = "#d4be98";
         };
       };
       # }}}
-
       key_bindings = [
-        { key="E"; mode="Vi"; action="Up";                             }
-        { key="M"; mode="Vi"; action="Left";                           }
-        { key="K"; mode="Vi"; action="ScrollLineDown"; mods="Control"; }
-        { key="K"; mode="Vi"; action="SemanticRightEnd";               }
-        { key="K"; mode="Vi"; action="WordRightEnd"; mods="Shift";     }
-        { key="I"; mode="Vi"; action="Right";                          }
-        { key="L"; mode="Vi"; action="ScrollToBottom";                 }
-        { key="L"; mode="Vi"; action="ToggleViMode";                   }
-        { key="N"; mode="Vi"; action="Down";                           }
-        { key="J"; mode="Vi"; action="SearchNext";                     }
-        { key="J"; mode="Vi"; action="SearchPrevious"; mods="Shift";   }
+        { key="M"; mode="Vi|~Search"; action="Left";                           }
+        { key="M"; mode="Vi|~Search"; action="High";           mods="Shift";   }
+        { key="N"; mode="Vi|~Search"; action="Down";                           }
+        { key="E"; mode="Vi|~Search"; action="Up";                             }
+        { key="I"; mode="Vi|~Search"; action="Right";                          }
+        { key="I"; mode="Vi|~Search"; action="Low";            mods="Shift";   }
+        { key="H"; mode="Vi|~Search"; action="Middle";         mods="Shift";   }
+        { key="J"; mode="Vi|~Search"; action="SearchNext";                     }
+        { key="J"; mode="Vi|~Search"; action="SearchPrevious"; mods="Shift";   }
+        { key="K"; mode="Vi|~Search"; action="SemanticRightEnd";               }
+        { key="K"; mode="Vi|~Search"; action="ScrollLineDown"; mods="Control"; }
+        { key="K"; mode="Vi|~Search"; action="WordRightEnd";   mods="Shift";   }
+        { key="L"; mode="Vi|~Search"; action="ScrollToBottom";                 }
+        { key="L"; mode="Vi|~Search"; action="ToggleViMode";                   }
       ];
     };
   };
