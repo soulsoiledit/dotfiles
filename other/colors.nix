ba104aeef2646834b7e924059aa3b7b7dd57d7fb
soulsoiledit <no.sleep410@passinbox.com>
2021-07-14 16:57:55 -0500

    added central theme file

--- other/colors.nix
+++ other/colors.nix
@@ -1,9 +1,68 @@
-{
-  theme = themes.amora;
-  themes = {
-    amora = {
-      background = "#2a2331";
+rec {
+  theme = amora;
+
+  gruvbox-material = {
+    background = "#1d2021";
+    foreground = "#d4be98";
+    selection = "#3c3836";
+    comment = "#7c6f64";
+    highlight = "#a9b665";
+
+    ansi = {
+      normal = {
+        black = "#1d2021";
+        red = "#ea6962";
+        green = "#a9b665";
+        yellow = "#d8a657";
+        blue = "#7daea3";
+        magenta = "#d3869b";
+        cyan = "#89b482";
+        white = "#d4be98";
+      };
+
+      bright = {
+        black = "#7c6f64";
+        red = "#ea6962";
+        green = "#a9b665";
+        yellow = "#d8a657";
+        blue = "#7daea3";
+        magenta = "#d3869b";
+        cyan = "#89b482";
+        white = "#d4be98";
+      };
     };
-  }
+  };
+
+  amora = {
+    background = "#2a2331";
+    foreground = "#dedbeb";
+    selection = "#634e75";
+    comment = "#634e75";
+    highlight = "#edabd2";
+
+    ansi = {
+      normal = {
+        black = "#211c26";
+        red = "#ed3f7f";
+        green = "#a2baa8";
+        yellow = "#eacac0";
+        blue = "#9985d1";
+        magenta = "#e68ac1";
+        cyan = "#aabae7";
+        white = "#dedbeb";
+      };
+
+      bright = {
+        black = "#634e75";
+        red = "#fb5c8e";
+        green = "#bfd1c3";
+        yellow = "#f0ddd8";
+        blue = "#b4a4de";
+        magenta = "#edabd2";
+        cyan = "#c4d1f5";
+        white = "#edebf7";
+      };
+    };
+  };
 }
 
