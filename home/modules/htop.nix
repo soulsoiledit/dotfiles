82872f65a09828db1667315397549533fd02e383
soulsoiledit <creepachu21@gmail.com>
2021-07-14 16:56:51 -0500

    updated to new config syntax

--- home/modules/htop.nix
+++ home/modules/htop.nix
@@ -1,33 +1,26 @@
 {
   programs.htop = {
     enable = true;
-    treeView = true;
-    headerMargin = false;
-    cpuCountFromZero = true;
-    shadowOtherUsers = true;
-    showProgramPath = false;
+    settings = {
+      tree_view = true;
+      header_margin = false;
+      cpu_count_from_zero = true;
+      shadow_other_users = true;
+      show_program_path = false;
+      highlight_base_name = true;
+      highlight_threads = false;
 
-    highlightBaseName = true;
-    highlightThreads = false;
+      hide_threads = true;
+      hide_kernel_threads = true;
+      hide_userland_threads = true;
 
-    hideThreads = true;
-    hideKernelThreads = true;
-    hideUserlandThreads = true;
+      # PID M_RESIDENT PERCENT_MEM PERCENT_CPU TIME COMM
+      fields = [ 0 39 47 46 49 1 ];
 
-    sortKey = "M_RESIDENT";
-    fields = [ "PID" "M_RESIDENT" "PERCENT_MEM" "PERCENT_CPU" "TIME" "COMM" ];
-
-    meters = {
-      left = [
-        "LeftCPUs2"
-        "Memory"
-        "Swap"
-      ];
-      right = [
-        "RightCPUs2"
-        "Tasks"
-        "Uptime"
-      ];
+      left_meters = [ "LeftCPUs2" "Memory" "Swap" ];
+      left_meter_modes = [ 1 1 1 ];
+      right_meters = [ "RightCPUs2" "Tasks" "Uptime" ];
+      right_meter_modes = [ 1 2 2 ];
     };
   };
 }
