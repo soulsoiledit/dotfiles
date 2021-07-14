5adc024746f79cbd04e9ae438b7ef76f23dd91d4
soulsoiledit <no.sleep410@passinbox.com>
2021-07-14 16:58:56 -0500

    using packer.nvim for now

--- home/modules/neovim/default.nix
+++ home/modules/neovim/default.nix
@@ -7,51 +7,6 @@
     withNodeJs = true;
     extraConfig = lib.readFile ./init.vim;
 
-    plugins = with pkgs.vimPlugins; [
-      # User Intereface
-      vim-airline
-      nvim-web-devicons
-      rainbow
-      indentLine # Use indent-blankline
-      vim-startify
-
-      # Integration
-        # Git
-        git-messenger-vim
-        vim-fugitive
-        vim-signify # vim-gitgutter
-      # Efficiency
-        # Movement
-        vim-lastplace
-        fzf-vim
-        vim-sneak
-        # Commands
-        auto-pairs
-        vim-repeat
-        vim-surround
-        vim-commentary
-        vim-better-whitespace
-        vim-peekaboo
-        vim-easy-align
-        undotree
-
-        vim-abolish
-        targets-vim
-      # Development
-      # nvim-lspconfig nvim-lspsaga
-      # nvim-treesitter
-        vim-hexokinase # nvim-colorizer.lua
-      completion-nvim
-        completion-buffers
-        # completion-treesitter
-
-      # telescope-nvim
-      #   plenary-nvim
-      #   popup-nvim
-
-      polyglot
-      syntastic
-      vim-dispatch
     ];
   };
 }
