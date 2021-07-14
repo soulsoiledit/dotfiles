9d3413f081fd4ab269e14a005e2f1b1365b4c886
soulsoiledit <creepachu21@gmail.com>
2021-07-14 16:59:18 -0500

    added lsp servers and other packages

--- home/modules/neovim/default.nix
+++ home/modules/neovim/default.nix
@@ -6,7 +6,32 @@
     package = pkgs.neovim-nightly;
     withNodeJs = true;
     extraConfig = lib.readFile ./init.vim;
+    extraPackages = with pkgs; [
+      gcc ctags
+      code-minimap
+      ripgrep fd
 
+      nodePackages.bash-language-server
+      nodePackages.vscode-css-languageserver-bin
+      nodePackages.vscode-html-languageserver-bin
+      nodePackages.pyright
+      nodePackages.typescript
+      nodePackages.typescript-language-server
+      nodePackages.vim-language-server
+      nodePackages.vscode-json-languageserver
+      nodePackages.yaml-language-server
+      nodePackages.dockerfile-language-server-nodejs
+      sumneko-lua-language-server
+      solargraph
+      rnix-lsp
+      dart
+      pandoc
     ];
+
+    # plugins = with pkgs.vimPlugins; [
+    # ];
   };
+
+  programs.lazygit.enable = true;
+  programs.gh.enable = true;
 }
