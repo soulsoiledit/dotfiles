31f8637beecef34637922dbbf94fcb1da331fca8
soulsoiledit <no.sleep410@passinbox.com>
2022-10-04 23:12:53 -0500

    modified flake

--- flake.nix
+++ flake.nix
@@ -1,9 +1,11 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+
+    master.url = "github:nixos/nixpkgs";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-22.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -13,7 +15,7 @@
     }; 
   };
 
-  outputs = { self, nixpkgs, home-manager, asusctl, ... }: {
+  outputs = { self, nixpkgs, home-manager, asusctl, ... }@inputs: {
     nixosConfigurations.soilnix = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
@@ -23,38 +25,34 @@
 
         {
           nix.registry.nixpkgs.flake = nixpkgs;
-
-          nixpkgs.overlays = [ asusctl.overlays.default ];
-
-          services.asusd = {
-              enable = true;
-              enable-power-profiles-daemon = true;
-          };
-
-          services.supergfxd = {
-            enable = true;
-            gfx-mode = "Integrated";
-            gfx-managed = true;
-          };
+          nixpkgs.overlays = [ 
+            asusctl.overlays.default
+          ];
         }
       ];
     };
 
     homeConfigurations.soil = home-manager.lib.homeManagerConfiguration {
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      modules = [ 
-        ./home/home.nix
-      ];
+      system = "x86_64-linux";
+      username = "soil";
+      homeDirectory = "/home/soil";
+      stateVersion = "22.05";
+      configuration.imports = [
+          ./home/home.nix 
+          { nixpkgs.overlays = [ asusctl.overlays.default]; }
+        ];
+      extraSpecialArgs = { inherit inputs; };
     };
 
     apps.x86_64-linux.update-home = {
       type = "app";
       program = (nixpkgs.legacyPackages.x86_64-linux.writeScript "update-home" ''
-      set -euo pipefail
-      old_profile=$(nix profile list | grep home-manager-path | head -n1 | awk '{print $4}')
-      echo $old_profile
-      nix profile remove $old_profile
-      ${self.homeConfigurations.soil.activation-script}/activate || (${nixpkgs.legacyPackages.x86_64-linux.nix}/bin/nix profile install $old_profile)
+        set -euo pipefail
+        old_profile=$(nix profile list | grep home-manager-path | head -n1 | awk '{print $4}')
+        echo $old_profile
+        nix profile remove $old_profile
+        ${self.homeConfigurations.soil.activation-script}/activate || (${nixpkgs.legacyPackages.x86_64-linux.nix}/bin/nix profile install $old_profile)
     '').outPath;
     };
   };
