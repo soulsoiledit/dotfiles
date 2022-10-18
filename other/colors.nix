851e9efd6877508e05cfbc6fde42ce41e434f97f
soulsoiledit <no.sleep410@passinbox.com>
2022-10-17 22:01:35 -0500

    added catppuccin theme

--- other/colors.nix
+++ other/colors.nix
@@ -1,5 +1,5 @@
 rec {
-  theme = substrata;
+  theme = catppuccin;
 
   gruvbox-material = {
     background = "#1d2021";
@@ -65,99 +65,35 @@ rec {
     };
   };
 
-  substrata = {
-    name = "substrata";
-    background = "#191c25";
-    foreground = "#b5b4c9";
-    selection = "#2e313d";
-    comment = "#5b5f71";
-    highlight = "#5b5f71";
+  catppuccin = {
+    name = "catppuccin";
+    background = "#1e1e2e";
+    foreground = "#cdd6f4";
+    selection = "#45475a";
+    comment = "#585b70";
+    highlight = catppuccin.ansi.bright.magenta;
 
     ansi = {
       normal = {
-        black = "#191c25";
-        red = "#cf8164";
-        green = "#76a065";
-        yellow = "#ab924c";
-        blue = "#8296b0";
-        magenta = "#a18daf";
-        cyan = "#659ea2";
-        white = "#b5b4c9";
+        black = "#45475a"; # surface1
+        red = "#f38ba8"; # red
+        green = "#a6e3a1"; # green
+        yellow = "#f9e2af"; # yellow
+        blue = "#89b4fa"; # blue
+        magenta = "#f5c2e7"; # pink
+        cyan = "#94e2d5"; # teal
+        white = "#bac2de"; # subtext1
       };
 
       bright = {
-        black = "#5b5f71";
-        red = "#fe9f7c";
-        green = "#92c47e";
-        yellow = "#d2b45f";
-        blue = "#a0b9d8";
-        magenta = "#c6aed7";
-        cyan = "#7dc2c7";
-        white = "#f0ecfe";
-      };
-    };
-  };
-
-  tokyonight = {
-    background = "#1d2021";
-    foreground = "#d4be98";
-    selection = "#3c3836";
-    comment = "#7c6f64";
-    highlight = "#a9b665";
-
-    ansi = {
-      normal = {
-        black = "#1d2021";
-        red = "#ea6962";
-        green = "#a9b665";
-        yellow = "#d8a657";
-        blue = "#7daea3";
-        magenta = "#d3869b";
-        cyan = "#89b482";
-        white = "#d4be98";
-      };
-
-      bright = {
-        black = "#7c6f64";
-        red = "#ea6962";
-        green = "#a9b665";
-        yellow = "#d8a657";
-        blue = "#7daea3";
-        magenta = "#d3869b";
-        cyan = "#89b482";
-        white = "#d4be98";
-      };
-    };
-  };
-
-  catppuccino = {
-    background = "#1d2021";
-    foreground = "#d4be98";
-    selection = "#3c3836";
-    comment = "#7c6f64";
-    highlight = "#a9b665";
-
-    ansi = {
-      normal = {
-        black = "#1d2021";
-        red = "#ea6962";
-        green = "#a9b665";
-        yellow = "#d8a657";
-        blue = "#7daea3";
-        magenta = "#d3869b";
-        cyan = "#89b482";
-        white = "#d4be98";
-      };
-
-      bright = {
-        black = "#7c6f64";
-        red = "#ea6962";
-        green = "#a9b665";
-        yellow = "#d8a657";
-        blue = "#7daea3";
-        magenta = "#d3869b";
-        cyan = "#89b482";
-        white = "#d4be98";
+        black = "#585b70"; # surface2
+        red = "#f38ba8"; # red
+        green = "#a6e3a1"; # green
+        yellow = "#f9e2af"; # yellow
+        blue = "#89b4fa"; # blue
+        magenta = "#f5c2e7"; # pink
+        cyan = "#94e2d5"; # teal
+        white = "#a6adc8"; # subtext0
       };
     };
   };
