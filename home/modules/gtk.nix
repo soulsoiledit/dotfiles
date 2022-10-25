65b18d956f226dbfadbb27e227489be1f0e07bcb
soulsoiledit <creepachu21@gmail.com>
2022-10-25 00:20:28 -0500

    only add subdir...

--- home/modules/gtk.nix
+++ home/modules/gtk.nix
@@ -17,11 +17,11 @@
     gtk2.configLocation = "${config.xdg.configHome}/gtk-3.0/gtk2rc";
   };
 
-  xdg.dataFile."themes/".source = pkgs.fetchzip
+  xdg.dataFile."themes/Catppuccin-Mocha-Lavender".source = pkgs.fetchzip
     {
       name = "catpuccin-mocha-lavender";
       url = "https://github.com/catppuccin/gtk/releases/download/v-0.2.7/Catppuccin-Mocha-Lavender.zip";
       sha256 = "sha256-pPTtqzeUSBGdbtdgWVef4tKLafe2b5NQcjd48C0wpR8=";
       stripRoot = false;
-    };
+    } + "/Catppuccin-Mocha-Lavender";
 }
