294f91fb2a3beb780cdda3fb2ae4c85905d4ee20
soulsoiledit <no.sleep410@passinbox.com>
2022-10-25 00:15:42 -0500

    switched to catppuccin theme and papirus-dark

--- home/modules/gtk.nix
+++ home/modules/gtk.nix
@@ -1,19 +1,27 @@
-{ pkgs, config, ... }:
+{ pkgs, config, inputs, ... }:
 
 {
   gtk = {
     enable = true;
 
     theme = {
-      name = "Matcha-dark-azul";
-      package = pkgs.matcha-gtk-theme;
+      name = "Catppuccin-Mocha-Lavender";
+      package = pkgs.catppuccin-gtk;
     };
 
     iconTheme = {
-      name = "Papirus-Dark-Maia";
+      name = "Papirus-Dark";
       package = pkgs.papirus-maia-icon-theme;
     };
 
     gtk2.configLocation = "${config.xdg.configHome}/gtk-3.0/gtk2rc";
   };
+
+  xdg.dataFile."themes/".source = pkgs.fetchzip
+    {
+      name = "catpuccin-mocha-lavender";
+      url = "https://github.com/catppuccin/gtk/releases/download/v-0.2.7/Catppuccin-Mocha-Lavender.zip";
+      sha256 = "sha256-pPTtqzeUSBGdbtdgWVef4tKLafe2b5NQcjd48C0wpR8=";
+      stripRoot = false;
+    };
 }
