1b8e655ce5d3c4206884e2ecd3b95e78a9a86a3b
soulsoiledit <no.sleep410@passinbox.com>
2022-12-15 18:09:33 -0600

    using catppucin fish theme

--- home/modules/fish.nix
+++ home/modules/fish.nix
@@ -4,37 +4,39 @@
   programs.fish = {
     enable = true;
 
-    interactiveShellInit = ''
-      bind \cn fzf-cd-widget
+    interactiveShellInit =
+      # ts
+      ''
+        bind \cn fzf-cd-widget
 
-      set fish_color_autosuggestion brblack
-      set fish_color_cancel -r
-      set fish_color_command magenta
-      set fish_color_comment brblack
-      set fish_color_cwd green
-      set fish_color_cwd_root red
-      set fish_color_end blue
-      set fish_color_error yellow
-      set fish_color_escape cyan
-      set fish_color_history_current --bold
-      set fish_color_host normal
-      set fish_color_host_remote yellow
-      set fish_color_match black --background=blue
-      set fish_color_normal normal
-      set fish_color_operator cyan
-      set fish_color_param white
-      set fish_color_quote green
-      set fish_color_redirection magenta
-      set fish_color_search_match --background=brblack
-      set fish_color_selection -r
-      set fish_color_status red
-      set fish_color_user brgreen
-      set fish_color_valid_path --underline
-      set fish_pager_color_completion normal
-      set fish_pager_color_description green
-      set fish_pager_color_prefix --bold --underline
-      set fish_pager_color_progress black --background=green
-    '';
+        set fish_color_autosuggestion brblack
+        set fish_color_cancel -r
+        set fish_color_command magenta
+        set fish_color_comment brblack
+        set fish_color_cwd green
+        set fish_color_cwd_root red
+        set fish_color_end blue
+        set fish_color_error yellow
+        set fish_color_escape cyan
+        set fish_color_history_current --bold
+        set fish_color_host normal
+        set fish_color_host_remote yellow
+        set fish_color_match black --background=blue
+        set fish_color_normal normal
+        set fish_color_operator cyan
+        set fish_color_param white
+        set fish_color_quote green
+        set fish_color_redirection magenta
+        set fish_color_search_match --background=brblack
+        set fish_color_selection -r
+        set fish_color_status red
+        set fish_color_user brgreen
+        set fish_color_valid_path --underline
+        set fish_pager_color_completion normal
+        set fish_pager_color_description green
+        set fish_pager_color_prefix --bold --underline
+        set fish_pager_color_progress black --background=green
+      '';
 
     functions = {
       f = ''
@@ -66,7 +68,7 @@
       nc = "$EDITOR /etc/nixos/system/configuration.nix -c 'cd /etc/nixos'";
       ns = "sudo nixos-rebuild switch";
       hc = "$EDITOR /etc/nixos/home/home.nix -c 'cd /etc/nixos'";
-      hs = "nix run /etc/nixos#update-home";
+      hs = "home-manager switch --flake /etc/nixos";
       gc = "nix store gc";
 
       ls = "ls -h --color";
