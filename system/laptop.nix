c4d65b726e21a0b2460f7d3970e6b070f186ca56
soulsoiledit <creepachu21@gmail.com>
2022-12-15 18:10:00 -0600

    removed kbd rules, set settings to null to fix supergfxd

--- system/laptop.nix
+++ system/laptop.nix
@@ -8,25 +8,25 @@
 
   services.asusd = {
     enable = true;
-    enable-power-profiles-daemon = true;
+    enableUserService = true;
   };
+
+  services.power-profiles-daemon.enable = true;
 
   services.supergfxd = {
-    enable = true;
-    gfx-mode = "Hybrid";
-    gfx-managed = true;
+    settings = null;
   };
 
-  services.udev.extraRules = ''
-    ACTION=="add", ATTRS{idVendor}=="c2ab", ATTRS{idProduct}=="3939", RUN+="${
-      pkgs.writeShellScriptBin "kb-add" ''
-        DISPLAY=:0 HOME=/home/soil ${pkgs.xorg.xinput}/bin/xinput disable 'AT Translated Set 2 keyboard'
-      ''
-    }/bin/kb-add"
-    ACTION=="remove", ATTRS{idVendor}=="c2ab", ATTRS{idProduct}=="3939", RUN+="${
-      pkgs.writeShellScriptBin "kb-remove" ''
-        DISPLAY=:0 HOME=/home/soil ${pkgs.xorg.xinput}/bin/xinput enable 'AT Translated Set 2 keyboard'
-      ''
-    }/bin/kb-remove"
-  '';
+  # services.udev.extraRules = ''
+  #   ACTION=="add", ATTRS{idVendor}=="c2ab", ATTRS{idProduct}=="3939", RUN+="${
+  #     pkgs.writeShellScriptBin "kb-add" ''
+  #       DISPLAY=:0 HOME=/home/soil ${pkgs.xorg.xinput}/bin/xinput disable 'AT Translated Set 2 keyboard'
+  #     ''
+  #   }/bin/kb-add"
+  #   ACTION=="remove", ATTRS{idVendor}=="c2ab", ATTRS{idProduct}=="3939", RUN+="${
+  #     pkgs.writeShellScriptBin "kb-remove" ''
+  #       DISPLAY=:0 HOME=/home/soil ${pkgs.xorg.xinput}/bin/xinput enable 'AT Translated Set 2 keyboard'
+  #     ''
+  #   }/bin/kb-remove"
+  # '';
 }
