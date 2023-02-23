ad5e1b5be29e7018934bff7898de05b111342ffc
soulsoiledit <no.sleep410@passinbox.com>
2023-02-22 22:10:42 -0600

    remove master and stable

--- flake.nix
+++ flake.nix
@@ -2,17 +2,11 @@
   inputs = rec {
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     master.url = "github:nixos/nixpkgs";
-
     nixpkgs = unstable;
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = github:nix-community/home-manager;
       inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    spotify-adblock = {
-      url = "github:fufexan/dotfiles/c7cffa0be4d3a96e463ea1f2a896543ecd27042d";
-      inputs.nixpkgs.follows = "unstable";
     };
 
     catppuccin-discord = {
@@ -21,17 +15,19 @@
     };
 
     catppuccin-btop = {
-      url = "github:catppuccin/btop";
+      url = github:catppuccin/btop;
       flake = false;
     };
 
     catppuccin-zathura = {
-      url = "github:catppuccin/zathura";
+      url = github:catppuccin/zathura;
       flake = false;
     };
+
+    nur.url = github:nix-community/NUR;
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, nur, ... }@inputs: {
     nixosConfigurations.soilnix = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
@@ -48,13 +44,7 @@
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
         modules = [
           ./home/home.nix
-          {
-            nixpkgs.overlays = [
-              (self: super: {
-                # spotify = inputs.spotify-adblock.packages.x86_64-linux.orchis-theme;
-              })
-            ];
-          }
+          nur.hmModules.nur
         ];
         extraSpecialArgs = { inherit inputs; };
       };
