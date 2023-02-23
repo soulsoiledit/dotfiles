d8ff78ac555fafebef96668f3e14ed949bf6905c
soulsoiledit <creepachu21@gmail.com>
2023-02-22 22:11:25 -0600

    remove remove master unstable

--- flake.nix
+++ flake.nix
@@ -1,8 +1,6 @@
 {
   inputs = rec {
-    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    master.url = "github:nixos/nixpkgs";
-    nixpkgs = unstable;
+    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
 
     home-manager = {
       url = github:nix-community/home-manager;
