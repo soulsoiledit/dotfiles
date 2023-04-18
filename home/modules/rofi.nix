59a76237e14b64a085f6d7dbd31f2db1e6abc8f2
soulsoiledit <creepachu21@gmail.com>
2023-04-18 10:15:45 -0500

    formatting

--- home/modules/rofi.nix
+++ home/modules/rofi.nix
@@ -14,49 +14,51 @@
       drun-display-format = "{name}";
     };
 
-    theme = let
-      inherit (config.lib.formats.rasi) mkLiteral;
-      theme = (import ../../other/colors.nix).theme;
-    in {
-      "*" = {
-        background-color = mkLiteral "#00000000";
-        text-color = mkLiteral theme.background;
+    theme =
+      let
+        inherit (config.lib.formats.rasi) mkLiteral;
+        theme = (import ../../other/colors.nix).theme;
+      in
+      {
+        "*" = {
+          background-color = mkLiteral "#00000000";
+          text-color = mkLiteral theme.background;
+        };
+
+        "window" = {
+          background-color = mkLiteral theme.background;
+          text-color = mkLiteral theme.foreground;
+          width = mkLiteral "20%";
+        };
+
+        "prompt" = {
+          enabled = mkLiteral "true";
+          padding = mkLiteral "0.30% 1% 0% -0.5%";
+        };
+
+        "inputbar" = {
+          background-color = mkLiteral theme.highlight;
+          padding = mkLiteral "1.5%";
+        };
+
+        "listview" = {
+          padding = mkLiteral "0px";
+          lines = 5;
+        };
+
+        "element" = { padding = mkLiteral "1% 0.5%"; };
+
+        "element-icon" = {
+          size = mkLiteral "2em";
+        };
+
+        "element-text" = {
+          text-color = mkLiteral theme.foreground;
+          vertical-align = mkLiteral "0.5";
+          margin = mkLiteral "1% 0.25%";
+        };
+
+        "element selected" = { background-color = mkLiteral theme.selection; };
       };
-
-      "window" = {
-        background-color = mkLiteral theme.background;
-        text-color = mkLiteral theme.foreground;
-        width = mkLiteral "20%";
-      };
-
-      "prompt" = {
-        enabled = mkLiteral "true";
-        padding = mkLiteral "0.30% 1% 0% -0.5%";
-      };
-
-      "inputbar" = {
-        background-color = mkLiteral theme.highlight;
-        padding = mkLiteral "1.5%";
-      };
-
-      "listview" = {
-        padding = mkLiteral "0px";
-        lines = 5;
-      };
-
-      "element" = { padding = mkLiteral "1% 0.5%"; };
-
-      "element-icon" = { 
-        size = mkLiteral "2em"; 
-      };
-
-      "element-text" = {
-        text-color = mkLiteral theme.foreground;
-        vertical-align = mkLiteral "0.5";
-        margin = mkLiteral "1% 0.25%";
-      };
-
-      "element selected" = { background-color = mkLiteral theme.selection; };
-    };
   };
 }
