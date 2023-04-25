892ce41bd016b426558ca1f9910409ac1556c95f
soulsoiledit <no.sleep410@passinbox.com>
2023-04-25 11:50:03 -0500

    Remove unused imv keybinds

--- home/modules/imv.nix
+++ home/modules/imv.nix
@@ -2,17 +2,17 @@
 
 {
   home.packages = with pkgs; [ imv ];
-  xdg.configFile."imv/config".text = ''
-    [binds]
-    e = prev
-    n = next
-    gg = goto 0
-    <Shift+G> = goto -1
-    m = zoom -1
-    i = zoom 1
-    <Shift+M> = pan 50 0
-    <Shift+N> = pan 0 -50
-    <Shift+E> = pan 0 50
-    <Shift+I> = pan -50 0
-  '';
+  # xdg.configFile."imv/config".text = ''
+  #   [binds]
+  #   e = prev
+  #   n = next
+  #   gg = goto 0
+  #   <Shift+G> = goto -1
+  #   m = zoom -1
+  #   i = zoom 1
+  #   <Shift+M> = pan 50 0
+  #   <Shift+N> = pan 0 -50
+  #   <Shift+E> = pan 0 50
+  #   <Shift+I> = pan -50 0
+  # '';
 }
