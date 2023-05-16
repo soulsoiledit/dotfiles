784da3f53abc95143216a449311caebf6a6aeec1
soulsoiledit <creepachu21@gmail.com>
2023-05-15 19:16:10 -0500

    source catppuccin better

--- home/modules/alacritty.nix
+++ home/modules/alacritty.nix
@@ -1,11 +1,14 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 
 {
+
+  xdg.configFile."alacritty/catppuccin-mocha.yml".source = "${inputs.catppuccin-alacritty}/catppuccin-mocha.yml";
+
   programs.alacritty = {
     enable = true;
 
     settings = {
-      import = [ "~/.config/alacritty/catppuccin/catppuccin-mocha.yml" ];
+      import = [ "~/.config/alacritty/catppuccin-mocha.yml" ];
 
       cursor.vi_mode_style = "Beam";
       mouse.hide_when_typing = true;
@@ -14,59 +17,6 @@
         size = 8;
         normal.family = "FantasqueSansMono Nerd Font";
       };
-
-      #   colors = let theme = (import ../../other/colors.nix).theme;
-      #   in {
-      #     primary = {
-      #       background = theme.background;
-      #       foreground = theme.foreground;
-      #     };
-
-      #     cursor = {
-      #       text = theme.background;
-      #       cursor = theme.foreground;
-      #     };
-
-      #     search = {
-      #       matches = {
-      #         background = theme.ansi.bright.yellow;
-      #         foreground = theme.background;
-      #       };
-      #       focused_match = {
-      #         background = theme.highlight;
-      #         foreground = theme.background;
-      #       };
-      #     };
-
-      #     vi_mode_cursor = { cursor = theme.foreground; };
-
-      #     selection = {
-      #       foreground = "CellForeground";
-      #       background = theme.selection;
-      #     };
-
-      #     normal = {
-      #       black = theme.ansi.normal.black;
-      #       red = theme.ansi.normal.red;
-      #       green = theme.ansi.normal.green;
-      #       yellow = theme.ansi.normal.yellow;
-      #       blue = theme.ansi.normal.blue;
-      #       magenta = theme.ansi.normal.magenta;
-      #       cyan = theme.ansi.normal.cyan;
-      #       white = theme.ansi.normal.white;
-      #     };
-
-      #     bright = {
-      #       black = theme.ansi.bright.black;
-      #       red = theme.ansi.bright.red;
-      #       green = theme.ansi.bright.green;
-      #       yellow = theme.ansi.bright.yellow;
-      #       blue = theme.ansi.bright.blue;
-      #       magenta = theme.ansi.bright.magenta;
-      #       cyan = theme.ansi.bright.cyan;
-      #       white = theme.ansi.bright.white;
-      #     };
-      #   };
     };
   };
 }
