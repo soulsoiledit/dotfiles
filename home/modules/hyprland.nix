67151afd9e1d17ebbc20c2ce15cfde4e6d0576d2
soulsoiledit <aryankevat@proton.me>
2023-09-21 15:12:29 -0500

    Add hyprland

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -2,22 +2,32 @@
 
 {
   home.packages = with pkgs; [
-    eww-wayland
     grim
     slurp
     wl-clipboard
+    swaybg
+    bc
+    eww-wayland
+    hyprpicker
   ];
+
+  programs.eww = {
+    enable = false;
+    package = pkgs.eww-wayland;
+    # configDir = ./eww;
+  };
+
   wayland.windowManager.hyprland = {
     enable = true;
-    plugins = with pkgs; [
-      # hyprland-protocols
-      # hyprland-autoname-workspaces
-      # hyprdim
-    ];
+    # plugins = with pkgs; [
+    #   hyprland-protocols
+    #   hyprland-autoname-workspaces
+    #   hyprdim
+    # ];
     settings = {
       source = "./realconfig.conf";
     };
-    systemdIntegration = false;
+    systemdIntegration = true;
     xwayland.enable = true;
   };
 
@@ -32,36 +42,75 @@
         tray = {
           layer = "top";
           position = "top";
-          height = 20;
-          width = 200;
+          height = 25;
+          width = 150;
+          exclusive = true;
+          margin-top = 4;
+
           modules-center = [ "tray" ];
           "tray" = {
-            "icon-size" = 20;
-            spacing = 10;
+            icon-size = 20;
+            spacing = 5;
           };
         };
       };
       style = ''
         * {
           font-family: FantasqueSansM Nerd Font, monospace;
-          font-size: 12px;
+          font-size: 10px;
         }
 
         #tray, #waybar {
           background: none;
           border-bottom: none;
         }
-
-        tooltip * {
-          padding: 4px;
-        }
       '';
     };
-    # programs.xplr = {
-    #     enable = false;
-    # };
+  
+  programs.swaylock = {
+    enable = true;
+    package = pkgs.swaylock-effects;
+    settings = {
+      clock = true;
+      screenshots = true;
+      effect-blur = "5x5";
+      indicator = true;
+      ignore-empty-password = true;
+    };
+  };
 
-    # services.copyq = {
-    #     enable = true;
-    # };
+  services.clipman = {
+      enable = true;
+  };
+
+
+  # programs.foot = {
+  #   enable = true;
+  #
+  #   settings = {
+  #     main.font = "FantasqueSansMono Nerd Font:size=16";
+  #   };
+  # };
+
+  services.swayidle = {
+    enable = true;
+    systemdTarget = "graphical-session.target";
+    timeouts = [
+      {
+        timeout = 5;
+        command = "brightnessctl set 20%-";
+        resumeCommand = "brightnessctl set 20%+";
+      }
+
+      {
+        timeout = 360;
+        command = "swaylock";
+      }
+
+      {
+        timeout = 3600;
+        command = "systemctl suspend";
+      }
+    ];
+  };
 }
