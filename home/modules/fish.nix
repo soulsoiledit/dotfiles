f0ccc90eacd7a16645941b76550ca7faa756f3cc
soulsoiledit <no.sleep410@passinbox.com>
2023-09-26 23:35:48 -0500

    Move to fish.nix

--- home/modules/fish.nix
+++ home/modules/fish.nix
@@ -7,6 +7,24 @@
     nix-direnv.enable = true;
   };
 
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    PAGER = "bat";
+    MANPAGER = "nvim +Man!";
+
+    "_JAVA_OPTIONS" = "-Djava.util.prefs.userRoot='${config.xdg.configHome}/java'";
+    NPM_CONFIG_USERCONFIG = "${config.xdg.configHome}/npm/npmrc";
+
+    CARGO_HOME = "${config.xdg.dataHome}/cargo";
+    NIXOS_OZONE_WL = 1;
+  };
+
+  home.sessionPath = [
+    "/home/soil/.local/bin/"
+    "/home/soil/.local/share/npm/bin/"
+    "/home/soil/.local/share/cargo/bin/"
+  ];
+
   programs.fish = {
     enable = true;
 
