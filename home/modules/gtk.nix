b4785de54dd02109d36b82a179c29288738519d4
soulsoiledit <30585327+soulsoiledit@users.noreply.github.com>
2023-09-26 23:34:28 -0500

    Remove papirus-catppuccin

--- home/modules/gtk.nix
+++ home/modules/gtk.nix
@@ -16,12 +16,4 @@
 
     gtk2.configLocation = "${config.xdg.configHome}/gtk-3.0/gtk2rc";
   };
-
-  xdg.dataFile."themes/Catppuccin-Mocha-Lavender".source = pkgs.fetchzip
-    {
-      name = "catpuccin-mocha-lavender";
-      url = "https://github.com/catppuccin/gtk/releases/download/v-0.2.7/Catppuccin-Mocha-Lavender.zip";
-      sha256 = "sha256-pPTtqzeUSBGdbtdgWVef4tKLafe2b5NQcjd48C0wpR8=";
-      stripRoot = false;
-    } + "/Catppuccin-Mocha-Lavender";
 }
