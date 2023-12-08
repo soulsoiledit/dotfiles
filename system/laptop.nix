2ceb101fd856b84b94ef2d6ebb5ac8d28eedb77c
soulsoiledit <soulsoill@proton.me>
2023-12-08 14:05:10 -0600

    add wip kanata config

--- system/laptop.nix
+++ system/laptop.nix
@@ -29,4 +29,44 @@
       enable = true;
     };
   };
+
+  users.users.soil.extraGroups = [ "uinput" ];
+
+  services.kanata = {
+      # enable = true;
+      keyboards = {
+        main = {
+          config = /* kbd */ ''
+            (defsrc
+             grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
+             tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
+             caps a    s    d    f    g    h    j    k    l    ;    '    ret
+             lsft z    x    c    v    b    n    m    ,    .    /    rsft
+             lctl lmet lalt           spc            ralt rctl
+            )
+
+            (defalias
+              nav (layer-while-held nav)
+            )
+
+            (deflayer gallium
+             grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
+             tab  b    l    d    c    v    j    y    o    u    ,    [    ]    \
+             @nav n    r    t    s    g    p    h    a    e    i    /    ret
+             lsft q    m    w    z    x    k    f    '    ;    .    rsft
+             lctl lmet lalt           spc            ralt rctl
+            )
+
+            (deflayer nav
+             XX  XX    XX    XX    XX    XX    XX       XX       XX       XX      XX    XX    XX    XX
+             XX  XX    XX    XX    XX    XX    XX       XX       XX       XX      XX    XX    XX    XX
+             XX lmet    lalt    lctl    lsft    XX    left    down    up      rght  XX    XX    XX
+             XX XX    XX    XX    XX    XX    home    pgdn    pgup    end    XX    XX
+             XX XX XX           XX            XX XX
+            )
+          '';
+        };
+
+      };
+  };
 }
