48f112e2aea5b92a14465786e88a7cbde4245a77
soulsoiledit <no.sleep410@passinbox.com>
2024-01-03 02:00:45 -0600

    Cleanup system configuration

--- system/configuration.nix
+++ system/configuration.nix
@@ -11,58 +11,63 @@
 
   system.stateVersion = "22.05";
 
+  time.timeZone = "US/Central";
+
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
+    tmp.cleanOnBoot = true;
+
     loader = {
-      systemd-boot.enable = true;
-      systemd-boot.editor = false;
+      systemd-boot = {
+        enable = true;
+        editor = false;
+
+        netbootxyz.enable = true;
+      };
     };
+
     plymouth.enable = true;
   };
 
-  time.timeZone = "US/Central";
-
   nixpkgs.config.allowUnfree = true;
+
   nix = {
     package = pkgs.nixUnstable;
+    channel.enable = false;
     registry.nixpkgs.flake = inputs.nixpkgs;
-    nixPath = ["nixpkgs=${inputs.nixpkgs}"];
 
     gc = {
       automatic = true;
       dates = "weekly";
-      options = "--delete-older-than 7d";
+      options = "--delete-older-than +5";
     };
 
     settings = {
-      experimental-features = ["nix-command" "flakes" "ca-derivations"];
-
-      warn-dirty = false;
-      keep-outputs = true;
+      experimental-features = [
+        "flakes"
+        "nix-command"
+      ];
       auto-optimise-store = true;
     };
   };
 
+  zramSwap = {
+    enable = true;
+    algorithm = "zstd";
+    memoryPercent = 50;
+  };
+
   services = {
     openssh.enable = true;
-    journald.extraConfig = "SystemMaxUse=100M";
     udisks2.enable = true;
-  };
-  programs.dconf.enable = true;
-
-  fonts = {
-    enableDefaultPackages = true;
-    packages = with pkgs; [
-      noto-fonts-cjk
-      noto-fonts-extra
-      (nerdfonts.override {fonts = ["FantasqueSansMono"];})
-    ];
   };
 
   networking = {
     hostName = "soilnix";
-    networkmanager.enable = true;
-    # networkmanager.wifi.backend = "iwd";
+    networkmanager = {
+      enable = true;
+      wifi.backend = "iwd";
+    };
   };
 
   users.users.soil = {
@@ -72,6 +77,7 @@
       "wheel"
       "video"
       "networkmanager"
+      "plugdev"
       # "docker"
     ];
   };
@@ -81,20 +87,19 @@
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    pulse.enable = true;
-  };
-  hardware.bluetooth = {
-    enable = true;
-    settings = {
-      General = {
-        Experimental = true;
-      };
-    };
+    # pulse.enable = true;
   };
 
-  programs.hyprland.enable = true;
-  security.pam.services.swaylock = {};
+  hardware.bluetooth.enable = true;
+
+  fonts = {
+    enableDefaultPackages = true;
+
+    packages = with pkgs; [
+      (nerdfonts.override {fonts = ["FantasqueSansMono"];})
+      noto-fonts-cjk
+    ];
+  };
 
   services.greetd = {
     enable = true;
@@ -106,17 +111,16 @@
     };
   };
 
-  # virtualisation.docker.enable = true;
-
-  zramSwap = {
-    enable = true;
-    algorithm = "zstd";
-    memoryPercent = 50;
+  programs.regreet = {
+    # enable = true;
   };
 
-  programs.command-not-found.enable = false; # temporary
-  hardware.logitech.wireless.enable = true;
-  services.ratbagd.enable = true;
+  programs.hyprland.enable = true;
+  security.pam.services.swaylock = {};
 
+  programs.command-not-found.enable = false; # temporary
+  services.ratbagd.enable = true;
   hardware.opengl.driSupport32Bit = true;
+
+  # virtualisation.docker.enable = true;
 }
