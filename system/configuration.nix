353c721aa9b4104affc584308b90234905a694f2
soulsoiledit <no.sleep410@passinbox.com>
2024-01-24 23:12:01 -0600

    optimize settings for zram

--- system/configuration.nix
+++ system/configuration.nix
@@ -53,8 +53,14 @@
 
   zramSwap = {
     enable = true;
-    algorithm = "zstd";
     memoryPercent = 50;
+  };
+
+  boot.kernel.sysctl = {
+    "vm.swappiness" = 180;
+    "vm.watermark_boost_factor" = 0;
+    "vm.watermark_scale_factor" = 125;
+    "vm.page-cluster" = 0;
   };
 
   services = {
