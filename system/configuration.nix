b35088279f7cbaaf98c6c5d45404730904bc04cf
soulsoiledit <no.sleep410@passinbox.com>
2024-02-13 13:23:29 -0600

    mv laptop config under hosts

--- system/configuration.nix
+++ system/configuration.nix
@@ -1,26 +1,10 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
-
 {
   imports = [
-    # Include the results of the hardware scan.
-    ./hardware-configuration.nix
-
     ./core.nix
     ./network.nix
     ./nix.nix
     ./users.nix
     ./desktop.nix
     ./dev.nix
-
-    ./laptop.nix
   ];
 }
