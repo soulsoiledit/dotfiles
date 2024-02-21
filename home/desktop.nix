d480443ff533e78238dfcf0512e74740ec5c9568
soulsoiledit <no.sleep410@passinbox.com>
2024-02-21 01:13:11 -0600

    use variables for colors (foreshadowing)

--- home/desktop.nix
+++ home/desktop.nix
@@ -78,271 +78,285 @@
     #   hyprdim
     # ];
 
-    settings = {
-      # set monitor preferences
-      monitor = ",preferred,auto,auto";
+    settings =
+      let
+        gap = 2;
 
-      # set env variables
-      env = [
-        # setup multi-gpu support; use AMD iGPU only
-        "WLR_DRM_DEVICES,/dev/dri/card1"
+        gray = "6c7086";
+        accent1 = "cba6f7";
+        accent2 = "f38ba8";
+      in
+      {
+        # set monitor preferences
+        monitor = ",preferred,auto,auto";
 
-        # hint to UI frameworks to use wayland
-        "NIXOS_OZONE_WL,1"
-        "GDK_BACKEND,wayland,x11"
-        "QT_QPA_PLATFORM,wayland;xcb"
-        "SDL_VIDEODRIVER,wayland"
-        "CLUTTER_BACKEND,wayland"
+        # set env variables
+        env = [
+          # setup multi-gpu support; use AMD iGPU only
+          "WLR_DRM_DEVICES,/dev/dri/card1"
 
-        # set XDG spec explicitly
-        "XDG_CURRENT_DESKTOP,Hyprland"
-        "XDG_SESSION_TYPE,wayland"
-        "XDG_SESSION_DESKTOP,Hyprland"
-      ];
+          # hint to UI frameworks to use wayland
+          "NIXOS_OZONE_WL,1"
+          "GDK_BACKEND,wayland,x11"
+          "QT_QPA_PLATFORM,wayland;xcb"
+          "SDL_VIDEODRIVER,wayland"
+          "CLUTTER_BACKEND,wayland"
 
-      # execute at launch
-      exec-once = [
-        "swaybg -i ~/code/dotfiles/extra/darkspace.jpg"
-        "eww open bar --toggle"
-        "foot --server"
-        "firefox"
-        "rog-control-center"
-      ];
-
-      general = {
-        layout = "master";
-
-        gaps_in = 3;
-        gaps_out = 6;
-        border_size = 1;
-
-        cursor_inactive_timeout = 4;
-
-        "col.inactive_border" = "rgba(6c7086ff)";
-        "col.active_border" = "rgb(89dceb) rgb(a6e3a1) 45deg";
-      };
-
-      misc = {
-        disable_hyprland_logo = true;
-      };
-
-      group = {
-        "col.border_inactive" = "rgba(6c7086ff)";
-        "col.border_active" = "rgb(89dceb) rgb(a6e3a1) 45deg";
-
-        groupbar = {
-          gradients = false;
-          render_titles = false;
-          "col.inactive" = "rgba(89b4fa6c)";
-          "col.active" = "rgb(89b4fa) rgb(a6e3a1) 45deg";
-        };
-      };
-
-      layerrule = [
-        # Blur eww bar
-        "blur, gtk-layer-shell"
-
-        # Disable animations for slurp selection
-        "noanim, selection"
-      ];
-
-      decoration = {
-        rounding = 4;
-        drop_shadow = false;
-      };
-
-      animations = {
-        enabled = true;
-        # bezier = expoOut, 0.16, 1, 0.3, 1;
-
-        animation = [
-          "windows, 1, 10, default"
-          "windowsOut, 1, 5, default, popin 50%"
-
-          "fade, 1, 10, default"
-          "fadeOut, 1, 5, default"
-
-          "border, 1, 10, default"
-          "borderangle, 1, 10, default"
-          "workspaces, 1, 10, default, slide"
+          # set XDG spec explicitly
+          "XDG_CURRENT_DESKTOP,Hyprland"
+          "XDG_SESSION_TYPE,wayland"
+          "XDG_SESSION_DESKTOP,Hyprland"
         ];
-      };
 
-      # Input
-      input = {
-        # kb_file = ~/.local/share/xorg/xkb/gallium_angle
-        scroll_method = "2fg";
+        # execute at launch
+        exec-once = [
+          "swaybg -i ~/code/dotfiles/extra/darkspace.jpg"
+          "eww open bar --toggle"
+          "foot --server"
+          "firefox"
+          "rog-control-center"
+        ];
 
-        touchpad = {
-          # disable_while_typing = false;
-          # natural_scroll = false;
+        general = {
+          layout = "master";
+
+          gaps_in = gap;
+          gaps_out = gap * 2;
+          border_size = 1;
+
+          cursor_inactive_timeout = 4;
+
+          "col.inactive_border" = "rgb(${gray})";
+          "col.active_border" = "rgb(${accent1}) rgb(${accent2}) 45deg";
         };
-      };
 
-      gestures = {
-        workspace_swipe = true;
-        workspace_swipe_invert = false;
-      };
+        misc = {
+          disable_hyprland_logo = true;
+        };
 
-      master = {
-        new_is_master = false;
-      };
+        group = {
+          "col.border_inactive" = "rgb(${gray})";
+          "col.border_active" = "rgb(${accent1}) rgb(${accent2}) 45deg";
 
-      xwayland = {
-        force_zero_scaling = true;
-      };
+          groupbar = {
+            gradients = false;
+            render_titles = false;
+            "col.inactive" = "rgb(${gray})";
+            "col.active" = "rgb(${accent1}) rgb(${accent2}) 45deg";
+          };
+        };
 
-      # Window rules
-      windowrulev2 = [
-        # Float file picker dialog
-        "float, title:^(Open File)$"
-        "center, title:^(Open File)$"
-      ];
+        layerrule = [
+          # blur eww background
+          "blur, gtk-layer-shell"
+        ];
 
-      # Keybindings
-      "$mod" = "SUPER";
+        decoration = {
+          rounding = 4;
+          drop_shadow = false;
+        };
 
-      # Programs
-      "$screenshot" = "grimblast --freeze copysave area ~/pictures/screenshots/$(date +%F_%Hh%Mm%Ss).png";
-      "$volume_update" = ''eww update volume="$(~/.config/eww/scripts/volume.sh)"'';
-      "$brightness_update" = "eww update brightness=$(~/.config/eww/scripts/brightness.sh)";
+        animations = {
+          enabled = true;
+          # bezier = expoOut, 0.16, 1, 0.3, 1;
 
-      "$notify_kbd" = ''notify-send "Current keyboard led brightness" $(asusctl -k | rg ".* (\S+)" -r "\$1")'';
-      "$notify_led" = ''notify-send "Current keyboard led mode:" "$(rg '\s+current_mode: (\S+),$' -r '$1' /etc/asusd/aura.ron)"'';
+          animation = [
+            "windows, 1, 10, default"
+            "windowsOut, 1, 5, default, popin 50%"
 
-      bind = [
-        # Workspaces
-        # Switch workspaces with mainMod + [0-4]
-        # TODO: generate these automatically
-        "$mod, 1, workspace, 1"
-        "$mod, 2, workspace, 2"
-        "$mod, 3, workspace, 3"
-        "$mod, 4, workspace, 4"
-        "$mod, 5, workspace, 5"
+            "fade, 1, 10, default"
+            "fadeOut, 1, 5, default"
 
-        # Move active window to a workspace with mod + SHIFT + [0-9]
-        "$mod SHIFT, 1, movetoworkspacesilent, 1"
-        "$mod SHIFT, 2, movetoworkspacesilent, 2"
-        "$mod SHIFT, 3, movetoworkspacesilent, 3"
-        "$mod SHIFT, 4, movetoworkspacesilent, 4"
-        "$mod SHIFT, 5, movetoworkspacesilent, 5"
+            "border, 1, 10, default"
+            "borderangle, 1, 10, default"
+            "workspaces, 1, 10, default, slide"
+          ];
+        };
 
-        # scroll through existing workspaces with mouse
-        "$mod, mouse_down, workspace, e+1"
-        "$mod, mouse_up, workspace, e-1"
+        # Input
+        input = {
+          # kb_file = ~/.local/share/xorg/xkb/gallium_angle
+          scroll_method = "2fg";
 
-        # Move to last workspace
-        "$mod, Tab, workspace, previous"
+          touchpad = {
+            # disable_while_typing = false;
+          };
+        };
 
-        # Navigation
-        # Move focus with mod + arrow keys
-        "$mod, J, layoutmsg, cyclenext"
-        "$mod, K, layoutmsg, cycleprev"
-        "$mod, J, changegroupactive, b"
-        "$mod, K, changegroupactive, f"
+        gestures = {
+          workspace_swipe = true;
+          workspace_swipe_invert = false;
+        };
 
-        # Swap windows
-        "$mod, O, layoutmsg, swapwithmaster"
-        "$mod SHIFT, J, layoutmsg, swapnext"
-        "$mod SHIFT, K, layoutmsg, swapprev"
+        master = {
+          new_is_master = false;
+        };
 
-        # Change split ratio
-        "$mod, H, splitratio, -0.05"
-        "$mod, L, splitratio, +0.05"
+        xwayland = {
+          force_zero_scaling = true;
+        };
 
-        # Change number of master clients
-        "$mod, m, layoutmsg, removemaster"
-        "$mod, comma, layoutmsg, addmaster"
+        # Window rules
+        windowrulev2 = [
+          # Float file picker dialog
+          "float, title:^(Open File)$"
+          "center, title:^(Open File)$"
+        ];
 
-        # Windows
-        "$mod, W, killactive,"
-        "$mod, F, fullscreen, 1"
-        "$mod, T, togglefloating, 1"
-
-        # Hyprland
-        "$mod SHIFT, E, exit,"
+        # Keybindings
+        "$mod" = "SUPER";
 
         # Programs
-        "$mod, Return, exec, footclient"
-        "$mod, space, exec, fuzzel"
+        "$screenshot" = "grimblast --freeze copysave area ~/pictures/screenshots/$(date +%F_%Hh%Mm%Ss).png";
+        "$volume_update" = ''eww update volume="$(~/.config/eww/scripts/volume.sh)"'';
+        "$brightness_update" = "eww update brightness=$(~/.config/eww/scripts/brightness.sh)";
 
-        # Bar
-        "$mod, B, exec, eww open bar --toggle"
-        "$mod, B, exec, pkill waybar"
+        "$notify_kbd" = ''notify-send "Current keyboard led brightness" $(asusctl -k | rg ".* (\S+)" -r "\$1")'';
+        "$notify_led" = ''notify-send "Current keyboard led mode:" "$(rg '\s+current_mode: (\S+),$' -r '$1' /etc/asusd/aura.ron)"'';
 
-        # Screenshot
-        "$mod, S, exec, $screenshot"
+        bind = [
+          # Workspaces
+          # Switch workspaces with mainMod + [0-4]
+          # TODO: generate these automatically
+          "$mod, 1, workspace, 1"
+          "$mod, 2, workspace, 2"
+          "$mod, 3, workspace, 3"
+          "$mod, 4, workspace, 4"
+          "$mod, 5, workspace, 5"
 
-        # Clipboard
-        "$mod, p, exec, cliphist list | fuzzel -d --tabs 2 | cliphist decode | wl-copy"
+          # Move active window to a workspace with mod + SHIFT + [0-9]
+          "$mod SHIFT, 1, movetoworkspacesilent, 1"
+          "$mod SHIFT, 2, movetoworkspacesilent, 2"
+          "$mod SHIFT, 3, movetoworkspacesilent, 3"
+          "$mod SHIFT, 4, movetoworkspacesilent, 4"
+          "$mod SHIFT, 5, movetoworkspacesilent, 5"
 
-        # Lock
-        "$mod shift, l, exec, swaylock"
+          # scroll through existing workspaces with mouse
+          # "$mod, mouse_down, workspace, e+1"
+          # "$mod, mouse_up, workspace, e-1"
 
-        # Media
-        ",XF86AudioPlay, exec, playerctl play-pause"
-        ",XF86AudioPause, exec, playerctl play-pause"
-        ",XF86AudioPrev, exec, playerctl previous"
-        ",XF86AudioNext, exec, playerctl next"
+          # Move to last workspace
+          "$mod, Tab, workspace, previous"
 
-        # asus
-        ",XF86Launch1, exec, rog-control-center"
-        ",XF86Launch3, exec, asusctl led-mode --next-mode; $notify_led"
-        ",XF86Launch4, exec, asusctl profile --next"
-        ",XF86KbdBrightnessUp, exec, asusctl --next-kbd-bright; $notify_kbd"
-        ",XF86KbdBrightnessDown, exec, asusctl --prev-kbd-bright; $notify_kbd"
+          # Navigation
+          # Move focus with mod + arrow keys
+          "$mod, J, layoutmsg, cyclenext"
+          "$mod, K, layoutmsg, cycleprev"
+          "$mod, J, changegroupactive, b"
+          "$mod, K, changegroupactive, f"
 
-        # notifications
-        "ctrl, space, exec, makoctl dismiss"
-        "ctrl shift, space, exec, makoctl dismiss --all"
-        "ctrl shift, comma, exec, makoctl restore"
-        "ctrl shift, period, exec, makoctl invoke"
-      ];
+          # Swap windows
+          "$mod, O, layoutmsg, swapwithmaster"
+          "$mod SHIFT, J, layoutmsg, swapnext"
+          "$mod SHIFT, K, layoutmsg, swapprev"
 
-      bindm = [
-        # move/resize windows with mouse
-        "$mod, mouse:272, movewindow"
-        "$mod, mouse:273, resizewindow"
-      ];
-    };
+          # Change split ratio
+          "$mod, H, splitratio, -0.05"
+          "$mod, L, splitratio, +0.05"
 
-    extraConfig =
-      # python
-      ''
-        # Volume
-        bind = ,XF86AudioMute, exec, pamixer --toggle-mute; $volume_update
+          # Change number of master clients
+          "$mod, m, layoutmsg, removemaster"
+          "$mod, comma, layoutmsg, addmaster"
 
-        binde = ,XF86AudioRaiseVolume, exec, pamixer --increase 5; $volume_update
-        binde = ,XF86AudioLowerVolume, exec, pamixer --decrease 5; $volume_update
+          # Windows
+          "$mod, W, killactive,"
+          "$mod, F, fullscreen, 1"
+          "$mod, T, togglefloating, 1"
 
-        binde = SHIFT ,XF86AudioRaiseVolume, exec, pamixer --increase 1; $volume_update
-        binde = SHIFT ,XF86AudioLowerVolume, exec, pamixer --decrease 1; $volume_update
+          # Hyprland
+          "$mod SHIFT, E, exit,"
 
-        # Brightness
-        binde = ,XF86MonBrightnessUp, exec, brightnessctl set 20%+; $brightness_update
-        binde = ,XF86MonBrightnessDown, exec, brightnessctl set 20%-; $brightness_update
+          # Programs
+          "$mod, Return, exec, footclient"
+          "$mod, space, exec, fuzzel"
+
+          # Bar
+          "$mod, B, exec, eww open bar --toggle"
+          "$mod, B, exec, pkill waybar"
+
+          # Screenshot
+          "$mod, S, exec, $screenshot"
+
+          # Clipboard
+          "$mod, p, exec, cliphist list | fuzzel -d --tabs 2 | cliphist decode | wl-copy"
+
+          # Lock
+          "$mod shift, l, exec, swaylock"
+
+          # Media
+          ",XF86AudioPlay, exec, playerctl play-pause"
+          ",XF86AudioPause, exec, playerctl play-pause"
+          ",XF86AudioPrev, exec, playerctl previous"
+          ",XF86AudioNext, exec, playerctl next"
+
+          "$mod, 0, exec, playerctl play-pause"
+          "$mod, minus, exec, playerctl previous"
+          "$mod, equal, exec, playerctl next"
+
+          # asus
+          ",XF86Launch1, exec, rog-control-center"
+          ",XF86Launch3, exec, asusctl led-mode --next-mode; $notify_led"
+          ",XF86Launch4, exec, asusctl profile --next"
+          ",XF86KbdBrightnessUp, exec, asusctl --next-kbd-bright; $notify_kbd"
+          ",XF86KbdBrightnessDown, exec, asusctl --prev-kbd-bright; $notify_kbd"
+
+          # notifications
+          "ctrl, space, exec, makoctl dismiss"
+          "ctrl shift, space, exec, makoctl dismiss --all"
+          "ctrl shift, comma, exec, makoctl restore"
+          "ctrl shift, period, exec, makoctl invoke"
+
+          # brightness
+          ",XF86MonBrightnessUp, exec, brightnessctl set 20%+; $brightness_update"
+          ",XF86MonBrightnessDown, exec, brightnessctl set 20%-; $brightness_update"
+
+          # volume
+          ",XF86AudioMute, exec, pamixer --toggle-mute; $volume_update"
+
+          "SHIFT, XF86AudioRaiseVolume, exec, pamixer --increase 1; $volume_update"
+          "SHIFT, XF86AudioLowerVolume, exec, pamixer --decrease 1; $volume_update"
+        ];
+
+        binde = [
+          ",XF86AudioRaiseVolume, exec, pamixer --increase 5; $volume_update"
+          ",XF86AudioLowerVolume, exec, pamixer --decrease 5; $volume_update"
+        ];
+
+        bindm = [
+          # move/resize windows with mouse
+          "$mod, mouse:272, movewindow"
+          "$mod, mouse:273, resizewindow"
+        ];
+      };
+  };
+
+  services.mako =
+    let
+      background = "#1e1e2e";
+      text = "#cdd6f4";
+      accent = "#89b4fa";
+      progress = "#313244";
+    in
+    {
+      enable = true;
+      defaultTimeout = 5000;
+
+      width = 250;
+      borderRadius = 4;
+
+      font = "Fantasque Sans Mono 10";
+      backgroundColor = "${background}";
+      textColor = "${text}";
+      borderColor = "${accent}";
+      progressColor = "over ${progress}";
+
+      extraConfig = ''
+        [urgency=high]
+        background-color=${accent}
+        text-color=${background}
+        border-color=${accent}
       '';
-  };
-
-  services.mako = {
-    enable = true;
-    backgroundColor = "#1e1e2e";
-    textColor = "#cdd6f4";
-    borderColor = "#89b4fa";
-    progressColor = "over #313244";
-    borderRadius = 4;
-    font = "FantasqueSansM Nerd Font 10";
-    defaultTimeout = 5000;
-
-    width = 250;
-
-    extraConfig = ''
-      [urgency=high]
-      background-color=#89b4fa
-      text-color=#1e1e2e
-      border-color=#89b4fa
-    '';
-  };
+    };
 
   # TODO: switch to ags?
   programs.waybar = {
