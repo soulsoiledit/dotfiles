bf0c045b283e3a106a64e8ba2001a6fb3ba6b51c
soulsoiledit <no.sleep410@passinbox.com>
2024-02-28 00:16:59 -0600

    add listener for battery changes
    uses upower for monitoring battery changes

--- home/eww/scripts/battery.sh
+++ home/eww/scripts/battery.sh
@@ -1,22 +1,29 @@
-acpi_output=$(acpi -b | rg -v 'unav')
-state=$(echo "$acpi_output" | rg -o ': (.*?),' -r '$1')
-percentage=$(echo "$acpi_output" | rg -o '(\d+)%' -r '$1')
-remaining=$(echo "$acpi_output" | rg -o ' (\d+:\d+)' -r '$1')
-wattage=$(cat /sys/class/power_supply/BAT0/power_now)
+battery() {
+	acpi_output=$(acpi -b | rg -v 'unav')
+	state=$(echo "$acpi_output" | rg -o ': (.*?),' -r '$1')
+	percentage=$(echo "$acpi_output" | rg -o '(\d+)%' -r '$1')
+	remaining=$(echo "$acpi_output" | rg -o ' (\d+:\d+)' -r '$1')
+	wattage=$(cat /sys/class/power_supply/BAT0/power_now)
 
-# TODO: add ramping symbols
-if [ "$state" == "full" ] || [ "$state" == "Not charging" ]; then
-	symbol="󰁹"
-elif [ "$state" == "Discharging" ]; then
-	symbol="󰁾"
-elif [ "$state" == "Charging" ]; then
-	symbol="󰂄"
-fi
+	# TODO: add ramping symbols
+	if [ "$state" == "full" ] || [ "$state" == "Not charging" ]; then
+		symbol="󰁹"
+	elif [ "$state" == "Discharging" ]; then
+		symbol="󰁾"
+	elif [ "$state" == "Charging" ]; then
+		symbol="󰂄"
+	fi
 
-jq -n \
-	--arg state "$state" \
-	--arg perc "$percentage" \
-	--arg remaining "$remaining" \
-	--arg wattage "$wattage" \
-	--arg symbol "$symbol" \
-	'{state, $state, percentage: $perc, remaining: $remaining, wattage: $wattage, symbol: $symbol}'
+	jq -n -c \
+		--arg state "$state" \
+		--arg perc "$percentage" \
+		--arg remaining "$remaining" \
+		--arg wattage "$wattage" \
+		--arg symbol "$symbol" \
+		'{state, $state, percentage: $perc, remaining: $remaining, wattage: $wattage, symbol: $symbol}'
+}
+
+battery
+upower -m | rg --line-buffered 'battery_BAT.' | while read -r _; do
+	battery
+done
