3419982c2721e83adb3a57693c479ce1f5093e57
soulsoiledit <soulsoill@proton.me>
2024-03-14 20:59:31 -0500

    add playerctl metadata tooltip

--- home/eww/scripts/playerctl.sh
+++
@@ -1,9 +0,0 @@
-playerctl -F status | while read -r status; do
-	if [ "$status" == "Playing" ]; then
-		symbol='󰏥'
-	else
-		symbol='󰐌'
-	fi
-
-	jq -n -c --arg status "$status" --arg symbol "$symbol" '{ status: $status, symbol: $symbol }'
-done
