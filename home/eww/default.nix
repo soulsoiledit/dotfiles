517fcb1b1751e8888426242a57ef9d21e8dd2cc3
soulsoiledit <no.sleep410@passinbox.com>
2024-03-14 21:01:38 -0500

    use initial values and access fields normally

--- home/eww/default.nix
+++ home/eww/default.nix
@@ -7,8 +7,7 @@
 
 {
   home.packages = with pkgs; [
-    # TODO: remove override when it is fixed
-    (inputs.eww-tray.packages.x86_64-linux.eww-wayland.override { withWayland = true; })
+    inputs.eww-tray.packages.x86_64-linux.eww
     acpi
     pavucontrol
     socat
@@ -17,7 +16,7 @@
   ];
 
   # used for making quick changes
-  xdg.configFile."eww".source = config.lib.file.mkOutOfStoreSymlink "/home/soil/code/dotfiles/home/eww";
+  xdg.configFile."eww".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/code/dotfiles/home/eww";
 
   # battery notifications service
   services.batsignal.enable = true;
