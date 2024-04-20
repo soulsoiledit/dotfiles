e840094b0a2fb636c1b9f06b2a778a1f6dc225d0
soulsoiledit <soulsoill@proton.me>
2024-04-20 11:58:01 -0500

    update hardware options after reinstall
    remove btrfs options

--- hosts/zephyrus/hardware.nix
+++ hosts/zephyrus/hardware.nix
@@ -23,13 +23,17 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/7d248662-b416-404c-a3f8-d24bce304498";
-    fsType = "btrfs";
+    device = "/dev/disk/by-uuid/4e1d7c36-b5d9-4d26-99c7-86a3fec321c9";
+    fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/008E-0232";
+    device = "/dev/disk/by-uuid/6E0A-B511";
     fsType = "vfat";
+    options = [
+      "fmask=0022"
+      "dmask=0022"
+    ];
   };
 
   swapDevices = [ ];
@@ -39,7 +43,7 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlan0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp5s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
