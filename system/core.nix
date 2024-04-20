f147eb8da998ffd9e302f9d10c652dc5d071e355
soulsoiledit <soulsoill@proton.me>
2024-04-20 12:19:00 -0500

    disable zram

--- system/core.nix
+++ system/core.nix
@@ -28,14 +28,14 @@
 
   # zram
   zramSwap = {
-    enable = true;
+    # enable = true;
   };
 
   boot.kernel.sysctl = {
-    "vm.swappiness" = 180;
-    "vm.watermark_boost_factor" = 0;
-    "vm.watermark_scale_factor" = 125;
-    "vm.page-cluster" = 0;
+    # "vm.swappiness" = 180;
+    # "vm.watermark_boost_factor" = 0;
+    # "vm.watermark_scale_factor" = 125;
+    # "vm.page-cluster" = 0;
   };
 
   environment.systemPackages = with pkgs; [
