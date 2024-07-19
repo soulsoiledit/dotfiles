3a69e71f36b9dbabcb001d781622f6ffbb0fe5de
soulsoiledit <no.sleep410@passinbox.com>
2024-07-18 23:21:47 -0500

    cleanup nix config

--- home/nix.nix
+++ home/nix.nix
@@ -14,17 +14,10 @@
     nix-index-database.comma.enable = true;
   };
 
-  # add support for xdg directories, not used system-wide for compat
   nix = {
     package = pkgs.nixStable;
     settings.use-xdg-base-directories = true;
   };
 
-  nixpkgs = {
-    config = {
-      allowUnfree = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/2942
-      allowUnfreePredicate = _: true;
-    };
-  };
+  nixpkgs.config.allowUnfree = true;
 }
