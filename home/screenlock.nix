8e100d896f3f531445a2063a4b92b4f97cee4043
soulsoiledit <no.sleep410@passinbox.com>
2024-07-18 23:21:55 -0500

    customize catppuccin swaylock

--- home/screenlock.nix
+++ home/screenlock.nix
@@ -1,16 +1,22 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   programs.swaylock = {
     enable = true;
     package = pkgs.swaylock-effects;
-    catppuccin.enable = true;
     settings = {
       clock = true;
       screenshots = true;
       effect-blur = "8x2";
       indicator = true;
       ignore-empty-password = true;
+
+      inside-color = lib.mkForce "1e1e2e";
+      inside-clear-color = lib.mkForce "1e1e2e";
+      inside-caps-lock-color = lib.mkForce "1e1e2e";
+      inside-ver-color = lib.mkForce "1e1e2e";
+      inside-wrong-color = lib.mkForce "1e1e2e";
+      separator-color = lib.mkForce "1e1e2e";
     };
   };
 }
