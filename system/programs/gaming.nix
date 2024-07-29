668d985aac83f445f87e6ce341b9600bc6cf913e
soulsoiledit <no.sleep410@passinbox.com>
2024-07-29 14:43:28 -0500

    add option for enabling gaming packages

--- system/programs/gaming.nix
+++ system/programs/gaming.nix
@@ -1,6 +1,17 @@
-{ pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
-  programs.steam.enable = true;
-  environment.systemPackages = with pkgs; [ mangohud ];
+  options = {
+    opts.gaming.enable = lib.mkEnableOption "enable gaming packages";
+  };
+
+  config = lib.mkIf config.opts.gaming.enable {
+    programs.steam.enable = true;
+    environment.systemPackages = with pkgs; [ mangohud ];
+  };
 }
