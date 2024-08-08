6080401b4b9a940a3507529e2e7ff5100cc5b6e9
soulsoiledit <no.sleep410@passinbox.com>
2024-08-08 14:38:20 -0500

    neovim: fix toggleterm mappings
    why did they even change?

--- home/editor/nvim/plugins/term.nix
+++ home/editor/nvim/plugins/term.nix
@@ -1,12 +1,28 @@
 {
-  programs.nixvim.plugins = {
-    toggleterm = {
-      enable = true;
-      settings = {
-        open_mapping = "[[<leader>t]]";
-        insert_mappings = false;
-        autochdir = true;
+  programs.nixvim = {
+    plugins = {
+      toggleterm = {
+        enable = true;
+        settings = {
+          open_mapping = "[[<leader>t]]";
+          insert_mappings = false;
+        };
       };
     };
+
+    keymaps = [
+      # escape for normal mode
+      {
+        mode = "t";
+        key = "<esc>";
+        action = "<C-\\><C-n>";
+      }
+      # ctrl-w prefix
+      {
+        mode = "t";
+        key = "<C-w>";
+        action = "<C-\\><C-n><C-w>";
+      }
+    ];
   };
 }
