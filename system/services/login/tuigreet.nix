23695f5278cc6f4dd24b3e78f9a0028ebdf833ae
soulsoiledit <no.sleep410@passinbox.com>
2024-08-11 03:05:44 -0500

    tuigreet: remove redundant option

--- system/services/login/tuigreet.nix
+++ system/services/login/tuigreet.nix
@@ -18,7 +18,7 @@
       vt = 7;
       settings = {
         default_session = {
-          command = "${lib.getExe pkgs.greetd.tuigreet} -t -r --remember-user-session --user-menu --asterisks --asterisks-char *";
+          command = "${lib.getExe pkgs.greetd.tuigreet} -t -r --remember-user-session --user-menu --asterisks";
         };
       };
     };
