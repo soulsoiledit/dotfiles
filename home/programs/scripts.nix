ab66673ed46ba8ecc0926e6a9e4ade18df84c600
soulsoiledit <no.sleep410@passinbox.com>
2024-09-03 23:46:14 -0500

    add module for various scripts to reuse

---
+++ home/programs/scripts.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    (writeShellScriptBin "volume_notify" ''
+      pamixer $@
+      volume=$(pamixer --get-volume)
+      notify-send "  $volume%" -h int:value:"$volume" -h string:x-dunst-stack-tag:volume
+    '')
+
+    (writeShellScriptBin "brightness_notify" ''
+      brightnessctl $@
+      brightness=$(brightnessctl info | rg -o "\d+%")
+      notify-send "󰃠  $brightness" -h int:value:"$brightness" -h string:x-dunst-stack-tag:brightness
+    '')
+
+    (writeShellScriptBin "profile_notify" ''
+      case $(powerprofilesctl get) in
+        'performance') powerprofilesctl set balanced;;
+        'balanced') powerprofilesctl set power-saver;;
+        'power-saver') powerprofilesctl set performance;;
+      esac;
+      notify-send "  Profile:" "$(powerprofilesctl get)" -h string:x-dunst-stack-tag:profile
+    '')
+
+    (writeShellScriptBin "niri_ws_update" ''
+      ${config.xdg.configHome}/eww/scripts/niri.sh update
+    '')
+  ];
+}
