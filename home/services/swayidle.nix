9a6f6c1110bd1e0a3a987136d6e92f0392b0613e
soulsoiledit <no.sleep410@passinbox.com>
2024-09-05 23:18:24 -0500

    swayidle: reenable service
    adjust timeouts and commands to fit niri

--- home/services/swayidle.nix
+++ home/services/swayidle.nix
@@ -10,12 +10,13 @@
     let
       swaylock = lib.getExe pkgs.swaylock-effects;
       loginctl = lib.getExe' pkgs.systemd "loginctl";
-      hyprctl = lib.getExe' config.wayland.windowManager.hyprland.finalPackage "hyprctl";
+      niri = lib.getExe' config.programs.niri.package "niri";
+      # hyprctl = lib.getExe' config.wayland.windowManager.hyprland.finalPackage "hyprctl";
       brightnessctl = lib.getExe pkgs.brightnessctl;
       systemctl = lib.getExe' pkgs.systemd "systemctl";
     in
     {
-      # enable = true;
+      enable = true;
 
       events = [
         {
@@ -30,21 +31,20 @@
 
       timeouts = [
         {
-          timeout = 600;
+          timeout = 300;
           command = "${brightnessctl} -s; ${brightnessctl} set 0%";
           resumeCommand = "${brightnessctl} -r";
         }
         {
-          timeout = 660;
-          command = "${hyprctl} dispatch dpms off";
-          resumeCommand = "${hyprctl} dispatch dpms on";
+          timeout = 600;
+          command = "${loginctl} lock-session";
         }
-        # {
-        #   timeout = 900;
-        #   command = "${loginctl} lock-session";
-        # }
         {
-          timeout = 900;
+          timeout = 720;
+          command = "${niri} msg action power-off-monitors";
+        }
+        {
+          timeout = 1800;
           command = "${systemctl} suspend";
         }
       ];
