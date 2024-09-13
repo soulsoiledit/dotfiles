eb4bf14327bee9381963eba64d62ad5f158ecf03
soulsoiledit <no.sleep410@passinbox.com>
2024-09-13 14:17:17 -0500

    nvim: add more mini.nvim modules

--- home/programs/nvim/plugins/editor.nix
+++ home/programs/nvim/plugins/editor.nix
@@ -1,84 +1,90 @@
 {
-  programs.nixvim = {
-    plugins = {
-      mini = {
-        enable = true;
-        modules = {
-          ai = { };
-          align = { };
-          basics = {
-            options.extra_ui = true;
-            navigation = {
-              windows = true;
-              move_with_alt = true;
-            };
+  programs.nixvim.plugins = {
+    mini = {
+      enable = true;
+      mockDevIcons = true;
+
+      modules = {
+        ai = { };
+        align = { };
+        move = { };
+        bracketed = { };
+        operators = { };
+
+        indentscope = { };
+        diff = {
+          view.style = "sign";
+        };
+        trailspace = { };
+
+        cursorword = { };
+        icons = { };
+
+        basics = {
+          options.extra_ui = true;
+          navigation = {
+            windows = true;
+            move_with_alt = true;
           };
-          bracketed = { };
-          cursorword = { };
-          icons = { };
-          trailspace = { };
+        };
 
-          pairs = {
-            mappings = {
-              # don't insert pair when typing before a character
-              "(" = {
-                action = "open";
-                pair = "()";
-                neigh_pattern = "[^\\][%s%)%]%}]";
-              };
-              "[" = {
-                action = "open";
-                pair = "[]";
-                neigh_pattern = "[^\\][%s%)%]%}]";
-              };
-              "{" = {
-                action = "open";
-                pair = "{}";
-                neigh_pattern = "[^\\][%s%)%]%}]";
-              };
+        pairs = {
+          mappings = {
+            # don't insert pair when typing before a character
+            "(" = {
+              action = "open";
+              pair = "()";
+              neigh_pattern = "[^\\][%s%)%]%}]";
+            };
+            "[" = {
+              action = "open";
+              pair = "[]";
+              neigh_pattern = "[^\\][%s%)%]%}]";
+            };
+            "{" = {
+              action = "open";
+              pair = "{}";
+              neigh_pattern = "[^\\][%s%)%]%}]";
+            };
 
-              "\"" = {
-                action = "closeopen";
-                pair = "\"\"";
-                neigh_pattern = "[^%w][^%w]";
-                register = {
-                  cr = false;
-                };
-              };
-              "'" = {
-                action = "closeopen";
-                pair = "''";
-                neigh_pattern = "[^%w][^%w]";
-                register = {
-                  cr = false;
-                };
-              };
-              "`" = {
-                action = "closeopen";
-                pair = "``";
-                neigh_pattern = "[^%w][^%w]";
-                register = {
-                  cr = false;
-                };
+            "\"" = {
+              action = "closeopen";
+              pair = "\"\"";
+              neigh_pattern = "[^%w][^%w]";
+              register = {
+                cr = false;
               };
             };
-          };
-
-          surround = {
-            mappings = {
-              add = "gsa";
-              delete = "gsd";
-              find = "gsf";
-              find_left = "gsF";
-              highlight = "gsh";
-              replace = "gsr";
-              update_n_lines = "gsn";
+            "'" = {
+              action = "closeopen";
+              pair = "''";
+              neigh_pattern = "[^%w][^%w]";
+              register = {
+                cr = false;
+              };
+            };
+            "`" = {
+              action = "closeopen";
+              pair = "``";
+              neigh_pattern = "[^%w][^%w]";
+              register = {
+                cr = false;
+              };
             };
           };
         };
+        surround = {
+          mappings = {
+            add = "gsa";
+            delete = "gsd";
+            find = "gsf";
+            find_left = "gsF";
+            highlight = "gsh";
+            replace = "gsr";
+            update_n_lines = "gsn";
+          };
+        };
       };
-
-      markdown-preview.enable = true;
     };
   };
 }
