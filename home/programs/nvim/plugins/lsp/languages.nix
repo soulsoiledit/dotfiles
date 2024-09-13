8da04ff9b0d8b2ce4ecbd186817163b502cdada6
soulsoiledit <no.sleep410@passinbox.com>
2024-09-13 14:26:02 -0500

    nvim: add plugins for markdown

--- home/programs/nvim/plugins/lsp/languages.nix
+++ home/programs/nvim/plugins/lsp/languages.nix
@@ -7,74 +7,80 @@
 }:
 
 {
-  programs.nixvim.plugins = {
-    lsp.servers = {
-      # INFO: remove when nixd has more features
-      nil-ls = {
-        enable = true;
-        settings.formatting.command = [ "${lib.getExe pkgs.nixfmt-rfc-style}" ];
+  programs.nixvim = {
+    plugins = {
+      lsp.servers = {
+        # INFO: remove when nixd has more features
+        nil-ls = {
+          enable = true;
+          settings.formatting.command = [ "${lib.getExe pkgs.nixfmt-rfc-style}" ];
+        };
+
+        nixd = {
+          enable = true;
+          settings =
+            let
+              getFlake = ''(builtins.getFlake "${inputs.self}")'';
+              user = "${config.home.username}";
+            in
+            {
+              formatting.command = [ "${lib.getExe pkgs.nixfmt-rfc-style}" ];
+              options.home-manager.expr = ''${getFlake}.homeConfigurations.${user}.options'';
+              diagnostic.suppress = [ "sema-escaping-with" ];
+            };
+        };
+
+        lua-ls.enable = true;
+
+        rust-analyzer = {
+          enable = true;
+          installCargo = true;
+          installRustc = true;
+        };
+        #
+        pylsp = {
+          enable = true;
+          package = pkgs.python3Packages.python-lsp-server;
+        };
+        #     # pylyzer.enable = true;
+        # pyright = {
+        #   enable = true;
+        #   package = pkgs.basedpyright;
+        # };
+        ruff.enable = true;
+        ruff-lsp.enable = true;
+
+        ruby-lsp.enable = true;
+        # solargraph.enable = true;
+
+        bashls.enable = true;
+        texlab.enable = true;
+        marksman.enable = true;
+
+        tsserver.enable = true;
+        cssls.enable = true;
+        html.enable = true;
+
+        jsonls.enable = true;
+        yamlls.enable = true;
+        taplo.enable = true;
+
+        zls.enable = true;
+        hls.enable = true;
+        jdt-language-server.enable = true;
+        clangd.enable = true;
       };
 
-      nixd = {
-        enable = true;
-        settings =
-          let
-            getFlake = ''(builtins.getFlake "${inputs.self}")'';
-            user = "${config.home.username}";
-          in
-          {
-            formatting.command = [ "${lib.getExe pkgs.nixfmt-rfc-style}" ];
-            options.home-manager.expr = ''${getFlake}.homeConfigurations.${user}.options'';
-            diagnostic.suppress = [ "sema-escaping-with" ];
-          };
-      };
+      # rustaceanvim.enable = true;
+      # haskell-tools.enable = true;
+      # ltex-extra.enable = true;
 
-      lua-ls.enable = true;
+      trouble.enable = true;
+      vimtex.enable = true;
 
-      rust-analyzer = {
-        enable = true;
-        installCargo = true;
-        installRustc = true;
-      };
-      #
-      pylsp = {
-        enable = true;
-        package = pkgs.python3Packages.python-lsp-server;
-      };
-      #     # pylyzer.enable = true;
-      # pyright = {
-      #   enable = true;
-      #   package = pkgs.basedpyright;
-      # };
-      ruff.enable = true;
-      ruff-lsp.enable = true;
-
-      ruby-lsp.enable = true;
-      # solargraph.enable = true;
-
-      bashls.enable = true;
-      texlab.enable = true;
-      marksman.enable = true;
-
-      tsserver.enable = true;
-      cssls.enable = true;
-      html.enable = true;
-
-      jsonls.enable = true;
-      yamlls.enable = true;
-      taplo.enable = true;
-
-      zls.enable = true;
-      hls.enable = true;
-      jdt-language-server.enable = true;
-      clangd.enable = true;
+      markdown-preview.enable = true;
     };
 
-    # rustaceanvim.enable = true;
-    # haskell-tools.enable = true;
-    # ltex-extra.enable = true;
-
-    trouble.enable = true;
-    vimtex.enable = true;
+    extraPlugins = with pkgs.vimPlugins; [ render-markdown ];
   };
 }
