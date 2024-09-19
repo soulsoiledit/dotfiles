e6aee31ee87d3e54187781a501bb19443037ab89
soulsoiledit <no.sleep410@passinbox.com>
2024-09-19 15:01:35 -0500

    cliphist: remove primary clipboard service

--- home/services/cliphist.nix
+++ home/services/cliphist.nix
@@ -24,23 +24,4 @@ in
       Wants = [ systemdTarget ];
     };
   };
-
-  # copies primary clipboard into cliphist
-  systemd.user.services.cliphist-primary = {
-    Unit = {
-      Description = "wl-copy";
-      Wants = [ systemdTarget ];
-      After = [ systemdTarget ];
-    };
-
-    Service = {
-      Type = "exec";
-      ExecStart = "${lib.getExe' pkgs.wl-clipboard "wl-paste"} --primary --watch cliphist store";
-      Restart = "on-failure";
-    };
-
-    Install = {
-      WantedBy = [ systemdTarget ];
-    };
-  };
 }
