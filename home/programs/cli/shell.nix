5b375e38cb79170a2621da1bc8556a9b4a08738d
soulsoiledit <no.sleep410@passinbox.com>
2024-09-26 13:48:23 -0500

    steam: don't always scale desktop
    only necessary when forcing 1x scaling in xwayland, which doesn't exist
    in xwayland-satellite yet

--- home/programs/cli/shell.nix
+++ home/programs/cli/shell.nix
@@ -15,7 +15,7 @@
     NIXOS_OZONE_WL = "1";
 
     # make steam ui readable
-    STEAM_FORCE_DESKTOPUI_SCALING = 1.5;
+    # STEAM_FORCE_DESKTOPUI_SCALING = 1.5;
 
     # set qt style
     QT_QPA_PLATFORMTHEME = config.qt.platformTheme.name;
