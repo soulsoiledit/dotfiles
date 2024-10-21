513579ee965599399fbf27d75a119dcee08aae3a
soulsoiledit <no.sleep410@passinbox.com>
2024-10-21 16:07:32 -0500

    neovim: cleanup

--- home/programs/neovim/ui/misc.nix
+++ home/programs/neovim/ui/misc.nix
@@ -4,7 +4,6 @@
       which-key.enable = true;
       rainbow-delimiters.enable = true;
       nvim-colorizer.enable = true;
-      trouble.enable = true;
 
       mini.modules = {
         indentscope = { };
