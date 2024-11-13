cd8241ac0b68359d5eb860aae13bc3a8f0d6c36d
soulsoiledit <no.sleep410@passinbox.com>
2024-11-13 14:26:06 -0600

    eww: fix script formatting

--- home/programs/widget/eww/scripts/brightness.sh
+++ home/programs/widget/eww/scripts/brightness.sh
@@ -1,15 +1,15 @@
 brightness() {
-	# brightness percentage
-	brightness=$(brightnessctl info | rg -o '(\d+)%' -r '$1')
+  # brightness percentage
+  brightness=$(brightnessctl info | rg -o '(\d+)%' -r '$1')
 
-	# brightness level
-	symbols=('󰽤' '' '' '' '' '󰃠')
-	symbol=${symbols[(((10#$brightness * 6 - 1) / 100))]}
+  # brightness level
+  symbols=('󰽤' '' '' '' '' '󰃠')
+  symbol=${symbols[(((10#$brightness * 6 - 1) / 100))]}
 
-	jq -n -c --arg symbol "$symbol" '{symbol: $symbol}'
+  jq -n -c --arg symbol "$symbol" '{symbol: $symbol}'
 }
 
 brightness
 udevadm monitor -u | rg --line-buffered "backlight" | while read -r _; do
-	brightness
+  brightness
 done
