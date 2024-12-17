c7ee8341b33bd206885bf2e97d2571c024d1ee62
soulsoiledit <no.sleep410@passinbox.com>
2024-12-16 22:49:31 -0600

    nvim: rename formatter configuration

---
+++ home/programs/neovim/code/format.nix
@@ -0,0 +1,26 @@
+{ pkgs, ... }:
+
+{
+  programs.nixvim = {
+    # for bashls
+    extraPackages = with pkgs; [ shfmt ];
+
+    plugins = {
+      lsp-format.enable = true;
+
+      none-ls = {
+        enable = true;
+        sources = {
+          formatting = {
+            prettier.enable = true;
+          };
+
+          completion = {
+            tags.enable = true;
+            spell.enable = true;
+          };
+        };
+      };
+    };
+  };
+}
