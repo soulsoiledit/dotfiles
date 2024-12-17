0f62df439aa559c4677b5327d1f8670e887cd691
soulsoiledit <no.sleep410@passinbox.com>
2024-12-16 22:51:22 -0600

    nvim: move grug

--- home/programs/neovim/edit/basic.nix
+++ home/programs/neovim/edit/basic.nix
@@ -1,15 +1,23 @@
+{ inputs, pkgs, ... }:
+
 {
   programs.nixvim.plugins = {
     lastplace.enable = true;
-    lazy.enable = true;
+    grug-far.enable = true;
+
     snacks = {
       enable = true;
-      settings.bigfile.enable = true;
+      # TODO: remove when snacks is updated
+      package = pkgs.vimUtils.buildVimPlugin {
+        pname = "snacks.nvim";
+        version = inputs.snacks-nvim.shortRev;
+        src = inputs.snacks-nvim;
+        meta.homepage = "https://github.com/folke/snacks.nvim/";
+      };
     };
 
     mini = {
       enable = true;
-
       modules = {
         basics = {
           options.extra_ui = true;
