d6d7276b1f847072c3a4e6cc379e32f765b105e8
soulsoiledit <no.sleep410@passinbox.com>
2024-12-28 20:55:03 -0600

    nvim: update blink config
    nvim: update blink config

--- home/programs/neovim/code/complete.nix
+++ home/programs/neovim/code/complete.nix
@@ -1,85 +1,108 @@
-{ inputs, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   programs.nixvim = {
     plugins = {
+      friendly-snippets.enable = true;
+
+      cmp-dictionary.enable = true;
+
+      lsp.capabilities = # lua
+        ''
+          capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
+        '';
+
+      blink-compat.enable = true;
+
       blink-cmp = {
         enable = true;
-        luaConfig.pre = # lua
-          ''
-            require('blink.compat').setup({
-              impersonate_nvim_cmp = true,
-            })
-          '';
         settings = {
           keymap = {
-            preset = "enter";
-            "<Tab>" = [
-              "select_next"
+            preset = "super-tab";
+            "<tab>" = {
+              __unkeyed-1.__raw = ''
+                function(cmp)
+                  if cmp.snippet_active() then
+                    return cmp.accept()
+                  else
+                    return cmp.select_next()
+                  end
+                end
+              '';
+              __unkeyed-2 = "snippet_forward";
+              __unkeyed-3 = "fallback";
+            };
+            "<s-tab>" = [
+              "select_prev"
               "snippet_forward"
               "fallback"
             ];
-            "<S-Tab>" = [
-              "select_prev"
-              "snippet_backward"
-              "fallback"
-            ];
           };
 
-          appearance.use_nvim_cmp_as_default = true;
-
-          completion.documentation = {
-            auto_show = true;
-            auto_show_delay_ms = 50;
+          appearance = {
+            use_nvim_cmp_as_default = true;
+            nerd_font_variant = "normal";
           };
 
-          # add calc and latex sources
+          completion = {
+            accept.auto_brackets.enabled = false;
+
+            keyword.range = "full";
+
+            list.selection = "auto_insert";
+
+            menu.draw = {
+              columns = [
+                {
+                  __unkeyed-1 = "label";
+                  __unkeyed-2 = "label_description";
+                  gap = 1;
+                }
+                [
+                  "kind_icon"
+                  "source_name"
+                ]
+              ];
+              treesitter = [ "lsp" ];
+            };
+
+            documentation = {
+              auto_show = true;
+              auto_show_delay_ms = 500;
+            };
+          };
+
           sources = {
-            completion.enabled_providers = [
+            default = [
               "lsp"
               "path"
               "snippets"
               "buffer"
-              "calc"
-              "latex_symbols"
+              "ripgrep"
+              "dictionary"
             ];
+
             providers = {
-              calc = {
-                name = "calc";
-                module = "blink.compat.source";
+              ripgrep = {
+                module = "blink-ripgrep";
+                name = "rg";
+                score_offset = -4;
+                opts.max_filesize = "1M";
               };
-              latex_symbols = {
-                name = "latex_symbols";
+              dictionary = {
+                name = "dictionary";
                 module = "blink.compat.source";
-                opts = {
-                  strategy = 2;
-                };
+                score_offset = -16;
+                opts.max_number_items = 16;
               };
             };
           };
         };
       };
-
-      friendly-snippets.enable = true;
-
-      cmp-calc.enable = true;
-      cmp-latex-symbols.enable = true;
-
-      # extend lsp capabilities
-      lsp.capabilities = # lua
-        ''
-          capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
-        '';
     };
 
-    # TODO: use blink.compat package and module when added
     extraPlugins = [
-      (pkgs.vimUtils.buildVimPlugin {
-        pname = "blink.compat";
-        version = inputs.blink-compat.shortRev;
-        src = inputs.blink-compat;
-        meta.homepage = "https://github.com/Saghen/blink.compat";
-      })
+      pkgs.vimPlugins.blink-ripgrep-nvim
     ];
   };
 }
