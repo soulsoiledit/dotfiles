b8280de20aac17bc63727b4d059f1b1cf75d8c82
soulsoiledit <no.sleep410@passinbox.com>
2025-01-06 22:47:44 -0600

    nvim: replace vimtex with texlab fully
    didn't really make use of the other features

--- home/programs/neovim/lang/tex.nix
+++ home/programs/neovim/lang/tex.nix
@@ -1,7 +1,34 @@
+{ lib, pkgs, ... }:
+
 {
   programs.nixvim.plugins = {
-    lsp.servers.texlab.enable = true;
+    lsp.servers.texlab = {
+      enable = true;
+      settings.texlab = {
+        build = {
+          executable = lib.getExe pkgs.tectonic;
+          args = [
+            "-X"
+            "compile"
+            "%f"
+            "--synctex"
+            "--keep-logs"
+            "--keep-intermediates"
+          ];
 
-    vimtex.enable = true;
+          onSave = true;
+          forwardSearchAfter = true;
+        };
+
+        forwardSearch = {
+          executable = lib.getExe pkgs.zathura;
+          args = [
+            "--synctex-forward"
+            "%l:1:%f"
+            "%p"
+          ];
+        };
+      };
+    };
   };
 }
