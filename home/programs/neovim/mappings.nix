44b7e9653e91e4c58e77e588a0c86c3e9afea3d6
soulsoiledit <no.sleep410@passinbox.com>
2025-01-11 17:25:27 -0600

    nvim: update binding desc

--- home/programs/neovim/mappings.nix
+++ home/programs/neovim/mappings.nix
@@ -1,32 +1,31 @@
 {
   programs.nixvim.keymaps = [
     {
-      # clear on escape
       mode = "n";
       key = "<esc>";
       action = "<cmd>noh<cr>";
-      options.desc = "clear search highlighting";
+      options.desc = "clear hlsearch";
     }
 
     {
       mode = "n";
       key = "<tab>";
       action = "<cmd>bnext<cr>";
-      options.desc = "next buffer";
+      options.desc = "buffer next";
     }
 
     {
       mode = "n";
       key = "<s-tab>";
       action = "<cmd>bprev<cr>";
-      options.desc = "prev buffer";
+      options.desc = "buffer prev";
     }
 
     {
       mode = "n";
       key = "<leader>x";
       action = "<cmd>bdelete<cr>";
-      options.desc = "close buffer";
+      options.desc = "buffer close";
     }
   ];
 }
