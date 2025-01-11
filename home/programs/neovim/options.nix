a5884281fc560740468d118e0533ffbc825b3556
soulsoiledit <no.sleep410@passinbox.com>
2025-01-11 17:24:49 -0600

    nvim: update options

--- home/programs/neovim/options.nix
+++ home/programs/neovim/options.nix
@@ -1,14 +1,22 @@
 {
   programs.nixvim = {
     opts = {
-      expandtab = true;
+      confirm = true;
+
       tabstop = 2;
       softtabstop = 2;
       shiftwidth = 2;
+      expandtab = true;
+      shiftround = true;
 
       relativenumber = true;
-      scrolloff = 4;
       wrap = true;
+
+      scrolloff = 4;
+      sidescrolloff = 8;
+
+      foldmethod = "indent";
+      foldlevelstart = 32;
 
       timeoutlen = 250;
     };
