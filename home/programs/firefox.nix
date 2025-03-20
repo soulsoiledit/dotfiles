465b8987f6e1f4ac1ec4639992aa1cbd69be1bfc
soulsoiledit <no.sleep410@passinbox.com>
2025-03-20 17:11:55 -0500

    firefox: remove pwa

--- home/programs/firefox.nix
+++ home/programs/firefox.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 
 with config.lib.stylix.colors.withHashtag;
 let
@@ -8,11 +8,8 @@ let
   accent = "#${config.opt.accent}";
 in
 {
-  home.packages = [ pkgs.firefoxpwa ];
-
   programs.firefox = {
     enable = true;
-    nativeMessagingHosts = [ pkgs.firefoxpwa ];
     profiles.${config.home.username} = {
       settings.toolkit.legacyUserProfileCustomizations.stylesheets = true;
 
