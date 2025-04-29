602ec5316a94bb6fefe8929f6137ea4d6e09ce55
soiledit <no.sleep410@passinbox.com>
2025-04-29 13:26:02 -0500

    boot: remove quieter boot

--- system/config/boot.nix
+++ system/config/boot.nix
@@ -8,17 +8,12 @@
       systemd-boot = {
         enable = true;
         configurationLimit = 16;
-        consoleMode = "max";
         editor = false;
       };
     };
 
-    # enable systemd in initramfs
     initrd.systemd.enable = true;
 
-    # make boot quieter
-    kernelParams = [ "quiet" ];
-    initrd.verbose = false;
     plymouth.enable = true;
   };
 }
