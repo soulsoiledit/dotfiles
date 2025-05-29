bc401d0ef0f28f6249b308e730b16837dcbbf30a
soiledit <no.sleep410@passinbox.com>
2025-05-28 22:21:47 -0500

    xwl: use xsettingsd

---
+++ home/services/xwayland.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  target = config.wayland.systemd.target;
+  service = "xwayland-satellite.service";
+in
+{
+  home.packages = [ pkgs.xwayland-satellite ];
+
+  # set DISPLAY
+  programs.niri.settings.environment.DISPLAY = ":0";
+
+  # copy service
+  xdg.configFile."systemd/user/${target}.wants/${service}".source =
+    "${pkgs.xwayland-satellite}/share/systemd/user/${service}";
+
+  # set dpi
+  services.xsettingsd = {
+    enable = true;
+    settings."Xft/DPI" = 192 * 1024;
+  };
+
+  systemd.user.services.xsettingsd = {
+    Service.Environment = [ "DISPLAY=:0" ];
+    Install.WantedBy = lib.mkForce [ service ];
+    Unit = {
+      After = [ service ];
+    };
+  };
+}
