bdc1b370f3d5785eeee94b51c56f90598e70e5fc
soiledit <no.sleep410@passinbox.com>
2025-07-17 17:20:00 -0500

    nvim: load nix generated config before vim.loader.enable
    prevents buggy caching with :source

--- home/programs/nvim/init.lua
+++ home/programs/nvim/init.lua
@@ -1,9 +1,9 @@
-vim.loader.enable()
-
 -- source nix generated config
 vim.cmd("source ~/.config/generated/nvim.lua")
 
 require("option")
 require("keymap")
 require("lsp")
+vim.loader.enable()
+
 require("lz.n").load("plugins")
