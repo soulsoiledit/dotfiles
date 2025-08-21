d251fe6b7cc52102d8e9f4376d16ddce72e506e2
soiledit <no.sleep410@passinbox.com>
2025-08-21 11:45:08 -0500

    greetd: use top level tuigreet package

--- system/services/greetd.nix
+++ system/services/greetd.nix
@@ -6,7 +6,7 @@
     settings = {
       default_session = {
         command = lib.strings.concatStringsSep " " [
-          (lib.getExe pkgs.greetd.tuigreet)
+          (lib.getExe pkgs.tuigreet)
           "--time"
           "--remember"
           "--remember-user-session"
