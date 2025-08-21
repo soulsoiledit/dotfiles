6db25dd22e56ed80c78b5cffa96a629318225673
soiledit <no.sleep410@passinbox.com>
2025-08-21 11:44:43 -0500

    mako: disable battery notifications

--- home/services/mako.nix
+++ home/services/mako.nix
@@ -4,21 +4,17 @@ let
   iconTheme = config.gtk.iconTheme;
 in
 {
-  services = {
-    mako = {
-      enable = true;
-      settings = {
-        default-timeout = 5000;
+  services.mako = {
+    enable = true;
+    settings = {
+      default-timeout = 5000;
 
-        width = 240;
-        border-size = 1;
-        border-radius = 4;
-        layer = "overlay";
+      width = 240;
+      border-size = 1;
+      border-radius = 4;
+      layer = "overlay";
 
-        icon-path = "${iconTheme.package}/share/icons/${iconTheme.name}";
-      };
+      icon-path = "${iconTheme.package}/share/icons/${iconTheme.name}";
     };
-
-    poweralertd.enable = true;
   };
 }
