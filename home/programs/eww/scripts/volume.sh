084258c77476a23aa20484f0074f7611123fad76
soiledit <no.sleep410@passinbox.com>
2025-09-05 15:57:42 -0500

    eww: add device name in volume tooltip

--- home/programs/eww/scripts/volume.sh
+++ home/programs/eww/scripts/volume.sh
@@ -6,18 +6,28 @@ pw-mon | while read -r line; do
     continue
   fi
 
-  out=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
-  if [[ $out == "$previous" ]]; then
+  while true; do
+    status=$(wpctl status -k)
+    # get the device name, volume, and muted
+    [[ $status =~ \*[^.]*\..([[:graph:][:blank:]]*)\[vol:.([[:digit:]]).([[:digit:]]{2}).?(MUTED)?\] ]]
+    status=${BASH_REMATCH[0]}
+    if [[ -z $status ]]; then
+      continue
+    fi
+    # trim trailing whitespace
+    device="${BASH_REMATCH[1]%"${BASH_REMATCH[1]##*[![:space:]]}"}"
+    percentage=$((10#${BASH_REMATCH[2]}${BASH_REMATCH[3]}))
+    muted=${BASH_REMATCH[4]}
+    break
+  done
+
+  # skip if status was the same
+  if [[ $status == "$previous" ]]; then
     continue
   fi
-  previous=$out
-
-  # parse the volume
-  [[ "$out" =~ ([0-9])\.([0-9]{2})(.\[MUTED\])? ]]
-  percentage=$((10#${BASH_REMATCH[1]}${BASH_REMATCH[2]}))
 
   # parse the muted status & assign symbols
-  if [[ -n ${BASH_REMATCH[3]} ]]; then
+  if [[ -n $muted ]]; then
     muted=true
     symbol=''
   else
@@ -26,7 +36,7 @@ pw-mon | while read -r line; do
     symbol=${symbols[((($percentage * 3 - 1) / 100))]}
   fi
 
-  # don't send a notification on the first iteration
+  # send notifications only after first iteration
   if [[ $previous ]]; then
     notify-send "$symbol  $percentage%" \
       -h int:value:"$percentage" \
@@ -35,7 +45,11 @@ pw-mon | while read -r line; do
 
   jq -nc \
     --arg symbol "$symbol" \
+    --arg device "$device" \
     --arg percentage "$percentage" \
     --arg muted "$muted" \
-    '{symbol: $symbol, percentage: $percentage, muted: $muted}'
+    '{symbol: $symbol, device: $device, percentage: $percentage, muted: $muted}'
+
+  # set previous
+  previous=$status
 done
