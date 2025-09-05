6c5c045af253cb497530e0ad9ea0dddb6bd7ebf0
soiledit <no.sleep410@passinbox.com>
2025-09-05 16:05:24 -0500

    niri: revert use of upstream portal config

--- home/programs/niri.nix
+++ home/programs/niri.nix
@@ -18,7 +18,13 @@ in
 
   xdg.portal = {
     enable = true;
-    configPackages = [ pkgs.niri ];
+    config.niri = {
+      default = "gnome;gtk";
+      "org.freedesktop.impl.portal.Access" = "gtk";
+      "org.freedesktop.impl.portal.Notification" = "gtk";
+      "org.freedesktop.impl.portal.Secret" = "gnome-keyring";
+      "org.freedesktop.impl.portal.FileChooser" = "gtk";
+    };
     extraPortals = [
       pkgs.xdg-desktop-portal-gnome
       pkgs.xdg-desktop-portal-gtk
