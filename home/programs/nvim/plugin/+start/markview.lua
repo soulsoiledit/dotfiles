b0ed3731c505dc2fdc0a23696d20e78d937e6806
soiledit <no.sleep410@passinbox.com>
2025-09-13 15:41:58 -0500

    nvim/markview: remove custom indentation function
    resolves issue with being able to distinguish normal and bulleted text
    and matches with typst

--- home/programs/nvim/plugin/+start/markview.lua
+++ home/programs/nvim/plugin/+start/markview.lua
@@ -1,15 +1,3 @@
-require("markview").setup({
-  markdown = {
-    list_items = {
-      shift_width = function(buffer, item)
-        local shiftwidth = vim.bo[buffer].shiftwidth
-        local prev = math.ceil(item.indent / shiftwidth + 1)
-        local final = math.ceil(item.indent / shiftwidth) * shiftwidth
-        return final / prev
-      end,
-    },
-  },
-})
-
+require("markview").setup()
 hl(0, "MarkviewListItemMinus", { link = "MarkviewPalette6Fg" })
 hl(0, "MarkviewListItemStar", { link = "MarkviewPalette2Fg" })
