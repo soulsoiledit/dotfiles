5de6811aad8db689e2611fdb8cf7ec2d4e785fa1
soiledit <no.sleep410@passinbox.com>
2025-12-28 21:13:44 -0600

    eww: fix volume script

--- home/programs/eww/scripts/volume.sh
+++ home/programs/eww/scripts/volume.sh
@@ -1,5 +1,5 @@
 previous=
-pw-mon | while read -r line; do
+pw-mon -o | while read -r line; do
   # ignore this check if it's the first iteration
   # check if the output doesn't have an indicator for modified volume
   if [[ $previous && ! $line =~ \*[[:blank:]]*params: ]]; then
@@ -7,18 +7,21 @@ pw-mon | while read -r line; do
   fi
 
   while true; do
-    status=$(wpctl status -k)
-    # get the device name, volume, and muted
-    [[ $status =~ \*[^.]*\..([[:graph:][:blank:]]*)\[vol:.([[:digit:]]).([[:digit:]]{2}).?(MUTED)?\] ]]
-    status=${BASH_REMATCH[0]}
-    if [[ -z $status ]]; then
-      continue
+    # get sink device name
+    device=$(wpctl inspect @DEFAULT_AUDIO_SINK@)
+    [[ $device =~ node.description.=.\"([[:graph:][:blank:]]*)\" ]]
+    device=${BASH_REMATCH[1]}
+
+    # get sink volume and mute status
+    volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
+    [[ $volume =~ ([[:digit:]]).([[:digit:]]{2}).?\[?(MUTED)?\]? ]]
+    percentage=$((10#${BASH_REMATCH[1]}${BASH_REMATCH[2]}))
+    muted=${BASH_REMATCH[3]}
+
+    status="${device} ${percentage} ${muted}"
+    if [[ $status ]]; then
+      break
     fi
-    # trim trailing whitespace
-    device="${BASH_REMATCH[1]%"${BASH_REMATCH[1]##*[![:space:]]}"}"
-    percentage=$((10#${BASH_REMATCH[2]}${BASH_REMATCH[3]}))
-    muted=${BASH_REMATCH[4]}
-    break
   done
 
   # skip if status was the same
@@ -26,8 +29,8 @@ pw-mon | while read -r line; do
     continue
   fi
 
-  # parse the muted status & assign symbols
-  if [[ -n $muted ]]; then
+  # assign the symbols for volume and mute
+  if [[ $muted ]]; then
     muted=true
     symbol=''
   else
