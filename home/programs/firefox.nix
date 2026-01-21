92a2c8045a5648ac90585a0a992c3097de9abe03
soiledit <no.sleep410@passinbox.com>
2026-01-20 20:10:20 -0600

    firefox: place config in new xdg directory

--- home/programs/firefox.nix
+++ home/programs/firefox.nix
@@ -5,6 +5,9 @@
 
   programs.firefox = {
     enable = true;
+    configPath = "${config.xdg.configHome}/mozilla/firefox";
     profiles.${config.home.username} = { };
   };
+
+  home.file.".mozilla/native-messaging-hosts".enable = false;
 }
