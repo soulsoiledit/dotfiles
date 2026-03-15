258c50d7f0d97ce7dae7171c840a12641c920c97
soiledit <no.sleep410@passinbox.com>
2026-03-15 14:22:01 -0500

    nvim: update biome root directory finder
    vim.fs.root() exists

--- home/programs/nvim/after/lsp/biome.lua
+++ home/programs/nvim/after/lsp/biome.lua
@@ -1,18 +1,23 @@
--- search for biome.json or biome.jsonc in current or parent directories
-local project_files = vim.fs.find({ "biome.json", "biome.jsonc" }, { upward = true, stop = "~" })
-local has_project_config = #project_files > 0
-
 ---@type vim.lsp.Config
 return {
   -- allow biome lsp-proxy to attach to standalone files
   workspace_required = false,
   root_dir = false,
-  root_markers = { { "package-lock.json", "yarn.lock", "pnpm-lock.yaml", "bun.lockb", "bun.lock" }, ".git" },
+  root_markers = {
+    { "package-lock.json", "yarn.lock", "pnpm-lock.yaml", "bun.lockb", "bun.lock", "deno.lock" },
+    ".git",
+  },
+
+  before_init = function(_params, config)
+    -- reset settings if biome.json or biome.jsonc found in current or parent directories
+    if vim.fs.root(0, { { "biome.json", "biome.jsonc" } }) and config.settings then
+      config.settings.biome = {}
+    end
+  end,
 
   settings = {
     biome = {
-      -- do not configure if project already has configuration
-      inlineConfig = has_project_config and {} or {
+      inlineConfig = {
         formatter = { indentStyle = "space" },
         html = { formatter = { enabled = true } },
       },
