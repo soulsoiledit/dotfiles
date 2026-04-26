caf8fc7440af19e0532b15ea60ef17eac978ec2f
soiledit <no.sleep410@passinbox.com>
2026-04-26 16:55:14 -0500

    zswap: bump pool size and swappiness

--- system/zswap.nix
+++ system/zswap.nix
@@ -1,6 +1,10 @@
 {
-  boot.kernel.sysfs.module.zswap.parameters = {
-    enabled = 1;
-    shrinker_enabled = 1;
+  boot.kernel = {
+    sysctl."vm.swappiness" = 160;
+    sysfs.module.zswap.parameters = {
+      enabled = 1;
+      max_pool_percent = 30;
+      shrinker_enabled = 1;
+    };
   };
 }
