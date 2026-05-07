0f9533b49a19fbc3b602e8692a2fe79edefe01a1
soiledit <no.sleep410@passinbox.com>
2026-05-06 23:28:37 -0500

    boot: systemd stage 1 boot is default now

--- system/boot.nix
+++ system/boot.nix
@@ -4,14 +4,11 @@
       efi.canTouchEfiVariables = true;
 
       timeout = 0;
-
       systemd-boot = {
         enable = true;
         configurationLimit = 16;
         editor = false;
       };
     };
-
-    initrd.systemd.enable = true;
   };
 }
