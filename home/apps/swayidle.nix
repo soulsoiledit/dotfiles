cc193915f92610cc0222b10086cd5dffc23236de
soiledit <no.sleep410@passinbox.com>
2026-05-20 20:49:12 -0500

    swayidle: reenable events

--- home/apps/swayidle.nix
+++ home/apps/swayidle.nix
@@ -1,32 +1,37 @@
 { lib, pkgs, ... }:
 
 let
-  loginctl = lib.getExe' pkgs.systemd "loginctl";
-  niri = lib.getExe pkgs.niri;
-  systemctl = lib.getExe' pkgs.systemd "systemctl";
+  lock = "${lib.getExe' pkgs.systemd "loginctl"} lock-session";
+  dpmsOn = "${lib.getExe pkgs.niri} msg action power-on-monitors";
+  dpmsOff = "${lib.getExe pkgs.niri} msg action power-off-monitors";
+  suspend = "${lib.getExe' pkgs.systemd "systemctl"} suspend";
 in
 {
   services.swayidle = {
     enable = true;
 
-    events."before-sleep" = "${loginctl} lock-session";
+    timeouts = [
+      {
+        timeout = 5 * 60;
+        command = lock;
+      }
 
-    # timeouts = [
-    #   {
-    #     timeout = 5 * 60;
-    #     command = "${niri} msg action power-off-monitors";
-    #     resumeCommand = "${niri} msg action power-on-monitors";
-    #   }
-    #
-    #   {
-    #     timeout = 10 * 60;
-    #     command = "${loginctl} lock-session";
-    #   }
-    #
-    #   {
-    #     timeout = 20 * 60;
-    #     command = "${systemctl} suspend";
-    #   }
-    # ];
+      {
+        timeout = 10 * 60;
+        command = dpmsOff;
+        resumeCommand = dpmsOn;
+      }
+
+      {
+        timeout = 30 * 60;
+        command = suspend;
+      }
+    ];
+
+    events = {
+      before-sleep = "${dpmsOff}; ${lock}";
+      after-resume = dpmsOn;
+      unlock = dpmsOn;
+    };
   };
 }
