b19af12f6b4b6dc6cde4353457a43dbdf7311009
soiledit <no.sleep410@passinbox.com>
2026-07-03 16:42:28 -0500

    zswap: use boot.zswap module

--- system/zswap.nix
+++ system/zswap.nix
@@ -1,10 +1,4 @@
 {
-  boot.kernel = {
-    sysctl."vm.swappiness" = 160;
-    sysfs.module.zswap.parameters = {
-      enabled = 1;
-      max_pool_percent = 30;
-      shrinker_enabled = 1;
-    };
-  };
+  boot.zswap.enable = true;
+  boot.kernel.sysctl."vm.swappiness" = 160;
 }
