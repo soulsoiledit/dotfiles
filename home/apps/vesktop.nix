0fa2621aae13e688fdc1df4da7f0c44fd2a83ee1
soiledit <no.sleep410@passinbox.com>
2026-07-07 17:20:26 -0500

    vesktop: revert resolved mitigation

--- home/apps/vesktop.nix
+++ home/apps/vesktop.nix
@@ -1,11 +1,6 @@
-{ pkgs, ... }:
-
 {
   stylix.targets.vesktop.enable = false;
   programs.vesktop = {
-    package = pkgs.vesktop.override {
-      pnpm_10_29_2 = pkgs.pnpm_10;
-    };
     enable = true;
     vencord.useSystem = true;
     vencord.settings.plugins.NoReplyMention = {
