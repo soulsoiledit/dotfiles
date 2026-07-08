0eb40038ce9375ce5414305cae0f24a31bd7ce2f
soiledit <no.sleep410@passinbox.com>
2026-07-08 14:09:06 -0500

    vesktop: add unindent plugin

--- home/apps/vesktop.nix
+++ home/apps/vesktop.nix
@@ -2,10 +2,16 @@
   stylix.targets.vesktop.enable = false;
   programs.vesktop = {
     enable = true;
-    vencord.useSystem = true;
-    vencord.settings.plugins.NoReplyMention = {
-      enabled = true;
-      inverseShiftReply = true;
+    vencord = {
+      useSystem = true;
+      settings.plugins = {
+        NoReplyMention = {
+          enabled = true;
+          inverseShiftReply = true;
+        };
+
+        Unindent.enabled = true;
+      };
     };
   };
 }
